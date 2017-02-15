e7efdc62f5d1bc2f196aa8342668d464e8f7baec
Will Geller <will.geller@gmail.com>
2017-02-15 14:30:25 -0700

    Add marker icon select to dashboardStore

--- Custom Dashboards/web/js/mango-3.4/components/dashboardStore/dashboardStore.js
+++ Custom Dashboards/web/js/mango-3.4/components/dashboardStore/dashboardStore.js
@@ -12,6 +12,69 @@ define(['angular', 'require'], function(angular, require) {
     function dashboardStoreController($scope, $timeout) {
         var $ctrl = this;
 
+        $ctrl.markerIcons = [
+            {
+                name: 'Red Dot',
+                image: 'img/map-markers/red-dot.png'
+            },
+            {
+                name: 'Red Small',
+                image: 'img/map-markers/mm_20_red.png'
+            },
+            {
+                name: 'Blue Dot',
+                image: 'img/map-markers/blue-dot.png'
+            },
+            {
+                name: 'Blue Small',
+                image: 'img/map-markers/mm_20_blue.png'
+            },
+            {
+                name: 'Green Dot',
+                image: 'img/map-markers/green-dot.png'
+            },
+            {
+                name: 'Green Small',
+                image: 'img/map-markers/mm_20_green.png'
+            },
+            {
+                name: 'Yellow Dot',
+                image: 'img/map-markers/yellow-dot.png'
+            },
+            {
+                name: 'Yellow Small',
+                image: 'img/map-markers/mm_20_yellow.png'
+            },
+            {
+                name: 'Purple Dot',
+                image: 'img/map-markers/purple-dot.png'
+            },
+            {
+                name: 'Purple Small',
+                image: 'img/map-markers/mm_20_purple.png'
+            },
+            {
+                name: 'Orange Dot',
+                image: 'img/map-markers/orange-dot.png'
+            },
+            {
+                name: 'Orange Small',
+                image: 'img/map-markers/mm_20_orange.png'
+            },
+            {
+                name: 'Brown Small',
+                image: 'img/map-markers/mm_20_brown.png'
+            },
+            {
+                name: 'Gray Small',
+                image: 'img/map-markers/mm_20_gray.png'
+            },
+            {
+                name: 'White Small',
+                image: 'img/map-markers/mm_20_white.png'
+            }
+        ];
+
         $ctrl.deleteDashboard = function() {
             $ctrl.dashboardList.dashboards = $ctrl.dashboardList.dashboards.filter(function(dashboard) {
                 return dashboard.name !== $ctrl.selectedDashboard.name;
