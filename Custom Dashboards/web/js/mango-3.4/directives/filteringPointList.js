84a91d73a3613c2d3f1c1e16a9c97696b18abbe7
Jared Wiltshire <jazdw@users.noreply.github.com>
2017-03-21 17:58:01 -0600

    Convert filtering point list to a component model

--- Custom Dashboards/web/js/mango-3.4/directives/filteringPointList.js
+++ Custom Dashboards/web/js/mango-3.4/directives/filteringPointList.js
@@ -25,126 +25,152 @@ define(['require', 'rql/query'], function(require, query) {
  * @usage
  * <ma-filtering-point-list ng-model="myPoint"></ma-filtering-point-list>
  */
-filteringPointList.$inject = ['Point', '$filter', '$injector', 'Translate', '$timeout'];
-function filteringPointList(Point, $filter, $injector, Translate, $timeout) {
+filteringPointList.$inject = [];
+function filteringPointList() {
     return {
         restrict: 'E',
-        require: 'ngModel',
-        designerInfo: {
-            translation: 'dashboards.v3.components.filteringPointList',
-            icon: 'filter_list',
-            category: 'dropDowns',
-            attributes: {
-                query: {}
-            }
-        },
-        scope: {
+        templateUrl: require.toUrl('./filteringPointList.html'),
+        scope: {},
+        controller: FilteringPointListController,
+        controllerAs: '$ctrl',
+        bindToController: {
             limit: '<?',
             autoInit: '<?',
-            pointXid: '@',
+            pointXid: '@?',
             pointId: '<?',
-            label: '@',
+            query: '@?',
+            label: '@?',
             listText: '&?',
             displayText: '&?',
             clientSideFilter: '<?'
         },
-        templateUrl: require.toUrl('./filteringPointList.html'),
-        link: filteringPointListPostLink
+        require: {
+            ngModelCtrl: 'ngModel'
+        },
+        designerInfo: {
+            translation: 'dashboards.v3.components.filteringPointList',
+            icon: 'filter_list',
+            category: 'dropDowns'
+        }
     };
+}
 
-    function filteringPointListPostLink($scope, $element, attrs, ngModelCtrl) {
-        if (!$scope.label)
-            $scope.label = Translate.trSync('dashboards.v3.app.searchBy', 'points', 'name or device');
-        
-        if ($scope.autoInit) {
-            if (!$scope.pointXid && !$scope.pointId) {
-                Point.rql({query: 'limit(1)'}).$promise.then(function(item) {
-                    $scope.setPoint(item[0]);
-                });
-            }
-        }
-        
-        $scope.$watch('pointXid', function(newValue, oldValue) {
-            if (!newValue) return;
-            
-            Point.get({xid: newValue}).$promise.then(function(item) {
-                $scope.setPoint(item);
-            });
-        });
+FilteringPointListController.$inject = ['Point', '$filter', 'Translate'];
+function FilteringPointListController(Point, $filter, Translate) {
+    this.Point = Point;
+    this.$filter = $filter;
+    this.Translate = Translate;
+}
 
-        $scope.$watch('pointId', function(newValue, oldValue) {
-            // jshint eqnull:true
-            if (newValue == null) return;
-            
-            Point.getById({id: newValue}).$promise.then(function(item) {
-                $scope.setPoint(item);
-            });
-        });
-        
-        ngModelCtrl.$render = function() {
-            $scope.selectedItem = this.$viewValue;
-        };
-        
-        $scope.setPoint = function(point) {
-            if (point) {
-                this.selectedItem = point;
-            }
-            ngModelCtrl.$setViewValue(this.selectedItem);
-        };
+FilteringPointListController.prototype.$onChanges = function(changes) {
+    if (changes.pointXid && (changes.pointXid.currentValue || !changes.pointXid.isFirstChange())) {
+        this.setByXid();
+    }
+    
+    if (changes.pointId && (changes.pointId.currentValue || changes.pointId.currentValue === 0 || !changes.pointId.isFirstChange())) {
+        this.setById();
+    }
+};
 
-        $scope.querySearch = function(inputText) {
-            var rqlQuery, queryString = '';
-            
-            if (inputText) {
-                rqlQuery = new query.Query();
-                var nameLike = new query.Query({name: 'like', args: ['name', '*' + inputText + '*']});
-                var deviceName = new query.Query({name: 'like', args: ['deviceName', '*' + inputText + '*']});
-                rqlQuery.push(nameLike);
-                rqlQuery.push(deviceName);
-                rqlQuery.name = 'or';
-            }
-
-            if (attrs.query) {
-                if (rqlQuery) {
-                    queryString = rqlQuery.toString();
+FilteringPointListController.prototype.$onInit = function() {
+    this.ngModelCtrl.$render = this.render.bind(this);
+    
+    if (!this.label)
+        this.label = this.Translate.trSync('dashboards.v3.app.searchBy', 'points', 'name or device');
+    
+    if (this.autoInit) {
+        if (!this.pointXid && !(this.pointId || this.pointId === 0)) {
+            this.Point.rql({query: 'limit(1)'}).$promise.then(function(items) {
+                if (items.length) {
+                    this.setViewValue(items[0]);
                 }
-                queryString = queryString + attrs.query;
-            } else {
-                var q = new query.Query();
-                if (rqlQuery)
-                    q.push(rqlQuery);
-
-                queryString = q.sort('deviceName', 'name')
-                    .limit($scope.limit || 150)
-                    .toString();
-            }
-            
-            return Point.rql({
-                rqlQuery: queryString
-            }).$promise.then(function(result) {
-                if ($scope.clientSideFilter) {
-                    return $filter('filter')(result, $scope.clientSideFilter);
-                } else {
-                    return result;
-                }
-            }, function() {
-                return [];
-            });
-        };
-        
-        if (!$scope.listText) {
-            $scope.listText = defaultText;
-        }
-        
-        if (!$scope.displayText) {
-            $scope.displayText = defaultText;
-        }
-        
-        function defaultText(opts) {
-            return opts.$point.deviceName + ' - ' + opts.$point.name;
+            }.bind(this));
         }
     }
-}
+
+    if (!this.listText) {
+        this.listText = defaultText;
+    }
+    
+    if (!this.displayText) {
+        this.displayText = defaultText;
+    }
+    
+    function defaultText(opts) {
+        return opts.$point.deviceName + ' - ' + opts.$point.name;
+    }
+};
+
+FilteringPointListController.prototype.render = function() {
+    this.selectedItem = this.ngModelCtrl.$viewValue || null;
+};
+
+FilteringPointListController.prototype.setViewValue = function(point) {
+    if (point) {
+        this.selectedItem = point;
+    }
+    this.ngModelCtrl.$setViewValue(this.selectedItem);
+};
+
+FilteringPointListController.prototype.querySearch = function(inputText) {
+    var rqlQuery, queryString = '';
+    
+    if (inputText) {
+        rqlQuery = new query.Query();
+        var nameLike = new query.Query({name: 'like', args: ['name', '*' + inputText + '*']});
+        var deviceName = new query.Query({name: 'like', args: ['deviceName', '*' + inputText + '*']});
+        rqlQuery.push(nameLike);
+        rqlQuery.push(deviceName);
+        rqlQuery.name = 'or';
+    }
+
+    if (this.query) {
+        if (rqlQuery) {
+            queryString = rqlQuery.toString();
+        }
+        queryString = queryString + this.query;
+    } else {
+        var q = new query.Query();
+        if (rqlQuery)
+            q.push(rqlQuery);
+
+        queryString = q.sort('deviceName', 'name')
+            .limit(this.limit || 150)
+            .toString();
+    }
+    
+    return this.Point.rql({
+        rqlQuery: queryString
+    }).$promise.then(function(result) {
+        if (this.clientSideFilter) {
+            return this.$filter('filter')(result, this.clientSideFilter);
+        } else {
+            return result;
+        }
+    }.bind(this), function() {
+        return [];
+    });
+};
+
+FilteringPointListController.prototype.setByXid = function() {
+    if (this.pointXid) {
+        this.Point.get({xid: this.pointXid}).$promise.then(function(item) {
+            this.setViewValue(item);
+        }.bind(this));
+    } else {
+        this.setViewValue(null);
+    }
+};
+
+FilteringPointListController.prototype.setById = function() {
+    if (this.pointId || this.pointId === 0) {
+        this.Point.getById({id: this.pointId}).$promise.then(function(item) {
+            this.setViewValue(item);
+        }.bind(this));
+    } else {
+        this.setViewValue(null);
+    }
+};
 
 return filteringPointList;
 
