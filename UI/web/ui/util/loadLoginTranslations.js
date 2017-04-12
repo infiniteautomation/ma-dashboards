6a6c90cfd33f3d439349f8987879a10e241dbfff
Jared Wiltshire <jazdw@users.noreply.github.com>
2017-04-12 16:53:49 -0600

    Move menu items into their own JS file

---
+++ UI/web/ui/util/loadLoginTranslations.js
@@ -0,0 +1,20 @@
+/**
+ * @copyright 2017 {@link http://infiniteautomation.com|Infinite Automation Systems, Inc.} All rights reserved.
+ * @author Jared Wiltshire
+ */
+
+define([], function() {
+'use strict';
+
+loadLoginTranslations.$inject = ['Translate', 'uiSettings', 'User', '$window'];
+function loadLoginTranslations(Translate, uiSettings, User, $window) {
+    return Translate.loadNamespaces('login').then(function(data) {
+        var user = User.current;
+        moment.locale((user && user.locale) || data.locale || $window.navigator.languages || $window.navigator.language);
+        moment.tz.setDefault(user ? user.getTimezone() : moment.tz.guess());
+    });
+}
+
+return loadLoginTranslations;
+
+});
