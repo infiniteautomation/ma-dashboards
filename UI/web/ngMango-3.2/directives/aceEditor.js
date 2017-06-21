5eb2935467f28f884dc69aa1f356341af07c699b
Jared Wiltshire <jazdw@users.noreply.github.com>
2017-06-21 13:58:37 -0600

    Move live editor to ace editor directive in ngMango

---
+++ UI/web/ngMango-3.2/directives/aceEditor.js
@@ -0,0 +1,135 @@
+/**
+ * @copyright 2017 {@link http://infiniteautomation.com|Infinite Automation Systems, Inc.} All rights reserved.
+ * @author Jared Wiltshire
+ */
+
+define(['require', 'angular'], function(require, angular) {
+'use strict';
+
+AceEditorController.$inject = ['$element', '$injector', '$templateRequest', '$sce', '$scope', '$timeout'];
+function AceEditorController($element, $injector, $templateRequest, $sce, $scope, $timeout) {
+    this.initialText = $element.data('htmlContent');
+    $element.removeData('htmlContent');
+    
+    var $ctrl = this;
+    if (MutationObserver) {
+        var observer = new MutationObserver(function() {
+            if ($ctrl.editor) {
+                $ctrl.editor.resize();
+            }
+        });
+        observer.observe($element[0], {
+            attributes: true,
+            attributeFilter: ['style']
+        });
+        $scope.$on('$destroy', function() {
+            observer.disconnect();
+        });
+    }
+    
+    if ($injector.has('maUiSettings')) {
+        this.uiSettings = $injector.get('maUiSettings');
+    }
+    this.$templateRequest = $templateRequest;
+    this.$sce = $sce;
+    this.$timeout = $timeout;
+}
+
+AceEditorController.prototype.$onInit = function() {
+    var $ctrl = this;
+    
+    this.ngModelCtrl.$render = function() {
+        $ctrl.setEditorText(this.$viewValue || '');
+    };
+    
+    this.aceConfig = {
+        useWrapMode : true,
+        showGutter: false,
+        showPrintMargin: false,
+        theme: this.theme || (this.uiSettings && this.uiSettings.codeTheme),
+        mode: this.mode || 'html',
+        onLoad: this.aceLoaded.bind(this),
+        onChange: this.aceChanged.bind(this)
+    };
+};
+
+AceEditorController.prototype.$onChanges = function(changes) {
+    if (changes.src) {
+        this.loadFromSrc();
+    }
+    if (changes.theme) {
+        this.setTheme();
+    }
+};
+
+AceEditorController.prototype.aceLoaded = function aceLoaded(editor) {
+    this.editor = editor;
+    editor.$blockScrolling = Infinity;
+
+    if (this.initialText) {
+        this.setEditorText(this.initialText);
+    }
+        
+    this.$timeout(function() {
+        this.editor.resize();
+    }.bind(this), 0);
+
+};
+
+AceEditorController.prototype.aceChanged = function aceChanged() {
+    var text = this.editor.getValue();
+    this.ngModelCtrl.$setViewValue(text);
+};
+
+AceEditorController.prototype.setEditorText = function(text) {
+    if (this.editor) {
+        this.editor.setValue(text, -1);
+    } else {
+        this.initialText = text;
+    }
+};
+
+AceEditorController.prototype.loadFromSrc = function loadFromSrc() {
+    if (!this.src) return;
+    var $ctrl = this;
+
+    var url = this.$sce.getTrustedResourceUrl(this.src);
+    this.$templateRequest(url).then(function(text) {
+        $ctrl.setEditorText(text);
+    });
+};
+
+AceEditorController.prototype.setTheme = function setTheme() {
+    if (this.editor && this.theme) {
+        this.editor.setTheme('ace/theme/' + this.theme);
+    }
+};
+
+AceEditor.$inject = [];
+function AceEditor() {
+    return {
+        restrict: 'E',
+        template: function($element, attrs) {
+            var htmlContent = $element.html().trim();
+            $element.empty();
+            if (htmlContent)
+                $element.data('htmlContent', htmlContent);
+            return '<div ui-ace="$ctrl.aceConfig"></div>';
+        },
+        require: {
+            'ngModelCtrl': 'ngModel'
+        },
+        scope: {},
+        controllerAs: '$ctrl',
+        bindToController: {
+            src: '@',
+            mode: '@',
+            theme: '@'
+        },
+        controller: AceEditorController
+    };
+}
+
+return AceEditor;
+
+}); // define
