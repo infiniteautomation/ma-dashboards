b956766da0aa98f406438ba94921b3d2cb05d501
Will Geller <will.geller@gmail.com>
2017-06-23 10:38:28 -0400

    add default map object to srcMap

--- UI/web/ngMango-3.2/directives/switchImg.js
+++ UI/web/ngMango-3.2/directives/switchImg.js
@@ -61,7 +61,8 @@ function switchImg() {
                 srcTrue: {type: 'choosefile', optional: true},
                 srcFalse: {type: 'choosefile', optional: true},
                 defaultSrc: {type: 'choosefile'},
-                srcMap: {type: 'string'},
+                srcMap: {type: 'string', defaultValue: "{'0': 'image1', '1':" +
+                " 'image2', '2': 'image3'}"},
                 value: {type: 'string'}
             }
         }
