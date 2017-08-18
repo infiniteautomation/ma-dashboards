07503c1aaead76a7aa01290932ad6f9bbe564708
Jared Wiltshire <jazdw@users.noreply.github.com>
2017-08-18 11:24:12 -0600

    Sort and merge segments

--- UI/web/ngMango/components/weeklySchedule/dailySchedule.js
+++ UI/web/ngMango/components/weeklySchedule/dailySchedule.js
@@ -24,12 +24,26 @@ class ActiveSegment {
     
     recalculate() {
         this.startLabel = moment.tz(this.startTime, 'UTC').format('LT');
-        this.endLabel = moment.tz(this.startTime + this.duration, 'UTC').format('LT');
+        this.endLabel = moment.tz(this.endTime, 'UTC').format('LT');
         this.durationLabel = moment.duration(this.duration).humanize();
         this.style = {
             left: (this.startTime / millisecondsInDay * 100) + '%',
             width: (this.duration / millisecondsInDay * 100) + '%'
         };
+    }
+    
+    setStartTime(startTime) {
+        this.startTime = startTime;
+        this.recalculate();
+    }
+    
+    setDuration(duration) {
+        this.duration = duration;
+        this.recalculate();
+    }
+
+    get endTime () {
+        return this.startTime + this.duration;
     }
 }
 
@@ -81,14 +95,47 @@ class DailyScheduleController {
     setViewValue() {
         const timestamps = [];
         
+        // sort by start time
+        this.activeSegments.sort((a, b) => {
+            return a.startTime - b.startTime;
+        });
+        
+        // remove any segments that start past end of day
+        // could get a segment that starts at millisecondsInDay due to rounding
+        this.activeSegments = this.activeSegments.filter((segment) => {
+            return segment.startTime < millisecondsInDay;
+        });
+        
+        for (let i = 0; i < this.activeSegments.length; i++) {
+            const segment = this.activeSegments[i];
+            
+            // ensure segment can't end past end of day
+            if (segment.endTime > millisecondsInDay) {
+                segment.setDuration(millisecondsInDay - segment.startTime);
+            }
+            
+            // merge overlapping segments
+            if ((i+1) < this.activeSegments.length) {
+                const nextSegment = this.activeSegments[i+1];
+                if (nextSegment.startTime <= segment.endTime) {
+                    // set the new duration if the overlapping segment extends the end time
+                    if (nextSegment.endTime > segment.endTime) {
+                        segment.setDuration(nextSegment.endTime - segment.startTime);
+                    }
+                    
+                    // remove next segment
+                    this.activeSegments.splice(i+1, 1);
+                }
+            }
+        }
+
         // convert segments to active/inactive timestamps
         this.activeSegments.forEach((segment) => {
             timestamps.push(segment.startTime);
-            
-            const inactiveTime = segment.startTime + segment.duration;
+
             // if it doesn't become inactive in this day we don't need to add a timestamp for it
-            if (inactiveTime < millisecondsInDay) {
-                timestamps.push(inactiveTime);
+            if (segment.endTime < millisecondsInDay) {
+                timestamps.push(segment.endTime);
             }
         });
         
@@ -111,7 +158,6 @@ class DailyScheduleController {
         const positionInDay = event.offsetX / event.currentTarget.clientWidth;
         const startTime = Math.round(positionInDay * millisecondsInDay / 60000) * 60000;
         const duration = 120 * 60 * 1000;
-        
         this.activeSegments.push(new ActiveSegment(startTime, duration));
 
         this.setViewValue();
