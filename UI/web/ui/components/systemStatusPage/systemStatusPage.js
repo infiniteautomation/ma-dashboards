f717362e6455161e5a91cb17941c81fd67bdd907
Will Geller <will.geller@gmail.com>
2017-09-12 17:28:08 -0400

    Move text-area to dialog

--- UI/web/ui/components/systemStatusPage/systemStatusPage.js
+++ UI/web/ui/components/systemStatusPage/systemStatusPage.js
@@ -6,14 +6,15 @@
 define(['angular', 'require'], function(angular, require) {
 'use strict';
 
-SystemStatusPageController.$inject = ['maSystemStatus', '$state', 'maUiMenu', '$mdMedia', 'maDialogHelper', 'maUiDateBar', '$scope', 'maRqlBuilder'];
-function SystemStatusPageController(systemStatus, $state, maUiMenu, $mdMedia, maDialogHelper, maUiDateBar, $scope, RqlBuilder) {
+SystemStatusPageController.$inject = ['maSystemStatus', '$state', 'maUiMenu', '$mdMedia', '$mdDialog', 'maDialogHelper',  'maUiDateBar', '$scope', 'maRqlBuilder'];
+function SystemStatusPageController(systemStatus, $state, maUiMenu, $mdMedia, $mdDialog, maDialogHelper, maUiDateBar, $scope, RqlBuilder) {
     this.systemStatus = systemStatus;
     this.$state = $state;
     this.$scope = $scope;
     this.menu = maUiMenu;
     this.$mdMedia = $mdMedia;
     this.maDialogHelper = maDialogHelper;
+    this.$mdDialog = $mdDialog;
     this.dateBar = maUiDateBar;
     this.RqlBuilder = RqlBuilder;
 
@@ -55,6 +56,15 @@ SystemStatusPageController.prototype.$onInit = function() {
     }, this.$scope);
 };
 
+/** Audit Context **/
+
+SystemStatusPageController.prototype.displayAuditContext = function() {
+    let selectedAuditEventContext = JSON.stringify(this.selectedAuditEvent[0].context);
+    let selectedAuditEventTitle = this.selectedAuditEvent[0].message;
+
+    this.showTextAreaDialog(selectedAuditEventTitle, selectedAuditEventContext);
+};
+
 SystemStatusPageController.prototype.updateAuditQuery = function() {
     // create a base rql query, will be of type 'and'
     const rootRql = new this.RqlBuilder();
@@ -83,30 +93,15 @@ SystemStatusPageController.prototype.updateAuditQuery = function() {
     });
 };
 
-SystemStatusPageController.prototype.displayAuditContext = function() {
-    this.selectedAuditEventContext = JSON.stringify(this.selectedAuditEvent[0].context);
-};
-
-
-SystemStatusPageController.prototype.getWorkItems = function() {
-    this.systemStatus.getWorkItemsQueueCounts().then((response) => {
-        this.workItemsQueueCounts = response.data;
-    });
-
-    this.systemStatus.getWorkItemsRunningStats().then((response) => {
-        this.workItemsRunningStats = response.data;
-    });
-
-    this.systemStatus.getWorkItemsRejectedStats().then((response) => {
-        this.workItemsRejectedStats = response.data;
-    });
-};
+/** Internal Metrics **/
 
 SystemStatusPageController.prototype.getInternalMetrics = function() {
     this.systemStatus.getInternalMetrics().then((response) => {
         this.internalMetrics = response.data;
     });
 };
+
+/** Server Info **/
 
 SystemStatusPageController.prototype.getLogFilesList = function() {
     this.systemStatus.getLogFilesList().then((response) => {
@@ -123,51 +118,11 @@ SystemStatusPageController.prototype.displayLogFile = function(filename) {
 
     this.systemStatus.getLogFile(filename).then((response) => {
         this.selectedLogContent = response.data;
+        this.showTextAreaDialog(this.selectedLogFile, this.selectedLogContent);
     });
 };
 
-SystemStatusPageController.prototype.copyLogToClipboard = function() {
-    document.querySelector('#logging-console').select();
-    document.execCommand('copy');
-
-    this.maDialogHelper.toastOptions({
-        text: this.selectedLogFile + ' copied to clipboard',
-        hideDelay: 4000
-    });
-};
-
-SystemStatusPageController.prototype.getThreads = function() {
-    this.systemStatus.getThreads().then((response) => {
-        this.threads = response.data;
-    });
-};
-
-SystemStatusPageController.prototype.showBlockedThreadDetails = function($event, thread) {
-    this.maDialogHelper.showBasicDialog($event, {
-        titleTr: 'ui.settings.systemStatus.blockedThreadDetails',
-        contentTemplate: '<p>lockOwnerName: ' + thread.lockOwnerName + '</p>' +
-                '<p>lockOwnerId: ' + thread.lockOwnerId + '</p>' +
-                '<p>className: ' +thread.lockInfo.className + '</p>' +
-                '<p>identityHashCode: ' +thread.lockInfo.identityHashCode + '</p>'
-    });
-};
-
-SystemStatusPageController.prototype.showStackTrace = function() {
-    this.selectedThreadStackTrace = '';
-    this.selectedThread[0].location.forEach( (item) => {
-        this.selectedThreadStackTrace += item.className + '.' + item.methodName + ':' + item.lineNumber + '\n';
-    });
-};
-
-SystemStatusPageController.prototype.copyStackTraceToClipboard = function() {
-    document.querySelector('#stack-trace').select();
-    document.execCommand('copy');
-
-    this.maDialogHelper.toastOptions({
-        text: this.selectedThread + ' stack trace copied to clipboard',
-        hideDelay: 4000
-    });
-};
+/** Server Info **/
 
 SystemStatusPageController.prototype.getSystemInfo = function() {
     this.systemStatus.getFullSystemInfo().then((response) => {
@@ -182,11 +137,84 @@ SystemStatusPageController.prototype.getPointCounts = function() {
     });
 };
 
+/** Threads **/
+
+SystemStatusPageController.prototype.getThreads = function() {
+    this.systemStatus.getThreads().then((response) => {
+        this.threads = response.data;
+    });
+};
+
+SystemStatusPageController.prototype.showBlockedThreadDetails = function($event, thread) {
+    this.maDialogHelper.showBasicDialog($event, {
+        titleTr: 'ui.settings.systemStatus.blockedThreadDetails',
+        contentTemplate: '<p>lockOwnerName: ' + thread.lockOwnerName + '</p>' +
+        '<p>lockOwnerId: ' + thread.lockOwnerId + '</p>' +
+        '<p>className: ' +thread.lockInfo.className + '</p>' +
+        '<p>identityHashCode: ' +thread.lockInfo.identityHashCode + '</p>'
+    });
+};
+
+SystemStatusPageController.prototype.showStackTrace = function() {
+    this.selectedThreadStackTrace = '';
+    this.selectedThread[0].location.forEach( (item) => {
+        this.selectedThreadStackTrace += item.className + '.' + item.methodName + ':' + item.lineNumber + '\n';
+    });
+    this.showTextAreaDialog(this.selectedThread[0].name, this.selectedThreadStackTrace);
+};
+
+/** Work Items **/
+
+SystemStatusPageController.prototype.getWorkItems = function() {
+    this.systemStatus.getWorkItemsQueueCounts().then((response) => {
+        this.workItemsQueueCounts = response.data;
+    });
+
+    this.systemStatus.getWorkItemsRunningStats().then((response) => {
+        this.workItemsRunningStats = response.data;
+    });
+
+    this.systemStatus.getWorkItemsRejectedStats().then((response) => {
+        this.workItemsRejectedStats = response.data;
+    });
+};
+
 SystemStatusPageController.prototype.isEmptyObj = function(obj) {
     if (obj === undefined) {
         return true;
     }
     return Object.keys(obj).length === 0;
+};
+
+
+SystemStatusPageController.prototype.showTextAreaDialog = function(title, textContent) {
+    let $this = this;
+    this.$mdDialog.show({
+        controller: function() {
+            this.title = title;
+            this.textContent = textContent;
+
+            this.copyToClipBoard = function() {
+                document.querySelector('#dialog-text-area').select();
+                document.execCommand('copy');
+
+                $this.maDialogHelper.toastOptions({
+                    text: 'Text copied to clipboard',
+                    hideDelay: 4000
+                });
+            };
+
+            this.ok = function() {
+                $this.$mdDialog.hide();
+            };
+            this.$mdMedia = $this.$mdMedia;
+        },
+        templateUrl: require.toUrl('./textAreaDialog.html'),
+        clickOutsideToClose: true,
+        escapeToClose: true,
+        controllerAs: '$ctrl',
+        bindToController: true
+    });
 };
 
     return {
