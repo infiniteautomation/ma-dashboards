b33162a6fc3a8872b3b953cf913de4839e5105d6
Jared Wiltshire <jazdw@users.noreply.github.com>
2017-09-27 12:52:51 -0600

    Add more functionality to ma-ui-state-params directive

--- UI/web/ui/directives/stateParams/stateParams.js
+++ UI/web/ui/directives/stateParams/stateParams.js
@@ -1,25 +1,59 @@
 /**
- * @copyright 2016 {@link http://infiniteautomation.com|Infinite Automation Systems, Inc.} All rights reserved.
+ * @copyright 2017 {@link http://infiniteautomation.com|Infinite Automation Systems, Inc.} All rights reserved.
  * @author Jared Wiltshire
  */
 
 define(['angular', 'require'], function(angular, require) {
 'use strict';
 
-var stateParams = function($stateParams) {
+stateParams.$inject = ['$stateParams', '$state', '$timeout'];
+function stateParams($stateParams, $state, $timeout) {
+
+    const $inject = ['$scope'];
+    class StateParamsController {
+        static get $inject() { return $inject; }
+        
+        constructor($scope) {
+            this.$scope = $scope;
+        }
+        
+        $onInit() {
+            this.stateParams = $stateParams;
+            this.notifyChange(true);
+        }
+        
+        $onChanges(changes) {
+            if (changes.updateParams && this.updateParams) {
+                $state.go('.', this.updateParams, {location: 'replace', notify: false});
+                this.stateParams = $stateParams;
+                this.notifyChange();
+            }
+        }
+        
+        notifyChange(init) {
+            if (!this.onChange) return;
+            
+            // wait for next digest cycle so $stateParams are updated
+            $timeout(() => {
+                this.$scope.$apply(() => {
+                    this.onChange({$stateParams: $stateParams, $onInit: init});
+                });
+            }, 0, false);
+        }
+    }
+
     return {
         restrict: 'E',
-        scope: {
-            stateParams: '='
+        scope: false,
+        controllerAs: '$ctrl',
+        bindToController: {
+            stateParams: '=?',
+            updateParams: '<?',
+            onChange: '&?'
         },
-        link: function($scope, $element) {
-            $element[0].style.display = 'none';
-            $scope.stateParams = $stateParams;
-        }
+        controller: StateParamsController
     };
-};
-
-stateParams.$inject = ['$stateParams'];
+}
 
 return stateParams;
 
