e633d19706260bf2c47a6ccce70d2afa6eac2da4
Jared Wiltshire <jazdw@users.noreply.github.com>
2017-11-17 10:45:43 -0700

    Fix message about limiting
    Conflicts:
    	UI/web/ngMango/services/pointValuesFactory.js

--- UI/web/ngMango/services/pointValuesFactory.js
+++ UI/web/ngMango/services/pointValuesFactory.js
@@ -74,7 +74,7 @@ function pointValuesFactory($http, $q, Util, MA_POINT_VALUES_CONFIG, $injector) 
                 	if (!this.lastToast || (now - this.lastToast) > 10000) {
                 		this.lastToast = now;
                 		maDialogHelper.toastOptions({
-                			textTr: ['ui.app.pointValuesLimited', [params.limit]],
+                			textTr: ['ui.app.pointValuesLimited', [params.limit || MA_POINT_VALUES_CONFIG.limit]],
                 			hideDelay: 10000,
                 			classes: 'md-warn'
                 		});
