ccde468d137bb67aa16e8ce6f588adae0ada0932
Jared Wiltshire <jazdw@users.noreply.github.com>
2018-01-31 17:58:28 -0700

    Work on bulk dp edit page

--- UI/web/ui/components/bulkDataPointEditPage/bulkDataPointEditPage.js
+++ UI/web/ui/components/bulkDataPointEditPage/bulkDataPointEditPage.js
@@ -9,29 +9,70 @@ define(['angular', 'require'], function(angular, require) {
 class BulkDataPointEditPageController {
     static get $$ngIsClass() { return true; }
     
-    static get $inject() { return ['maPoint', '$timeout', '$scope']; }
-    constructor(maPoint, $timeout, $scope) {
+    static get $inject() { return ['maPoint', '$timeout', '$scope', 'maDataPointTags']; }
+    constructor(maPoint, $timeout, $scope, maDataPointTags) {
         this.maPoint = maPoint;
         this.$timeout = $timeout;
         this.$scope = $scope;
+        this.maDataPointTags = maDataPointTags;
         
-        this.selectedPoints = [];
         this.numberOfRows = 25;
         this.pageNumber = 1;
         this.tableOrder = 'name';
-        this.selectDisabled = false;
-        this.selectAll = false;
-        this.selectAllIndeterminate = false;
-        
+
+        this.columns = [
+            {name: 'xid', label: 'XID', sortable: true, disabled: true},
+            {name: 'deviceName', label: 'Device', sortable: true, disabled: true},
+            {name: 'name', label: 'Name', sortable: true, disabled: true},
+            {name: 'readPermission', label: 'Read permission', sortable: true},
+            {name: 'setPermission', label: 'Set permission', sortable: true},
+            {name: 'unit', label: 'Unit', sortable: true},
+            {name: 'chartColor', label: 'Chart color', sortable: true},
+            {name: 'plotType', label: 'Plot type', sortable: true},
+            {name: 'rollup', label: 'Rollup type', sortable: true},
+            {name: 'templateXid', label: 'Template XID', sortable: true}
+        ];
+        this.selectedColumns = this.columns.slice();
+        this.availableTagsByKey = {};
+        this.availableTags = [];
+        this.selectedTags = [];
+
         this.selectedPointsChangedBound = (...args) => {
             this.selectedPointsChanged(...args);
         };
+        
+        this.reset();
     }
     
+    reset() {
+        this.selectedPoints = [];
+        this.selectAll = false;
+        this.selectAllIndeterminate = false;
+        this.updateBody = null;
+    }
+    
+    
     $onInit() {
+        this.maDataPointTags.keys().then(keys => {
+            keys.forEach(tagKey => this.addTagToAvailable(tagKey));
+        });
     }
     
     $onChanges(changes) {
+    }
+    
+    addTagToAvailable(tagKey) {
+        if (tagKey === 'device' || tagKey === 'name' || this.availableTagsByKey[tagKey]) return;
+        
+        const option = {
+            name: tagKey,
+            label: `Tag '${tagKey}'`
+        };
+        
+        this.availableTags.push(option);
+        this.availableTagsByKey[tagKey] = option;
+        
+        return option;
     }
     
     start(event) {
@@ -39,9 +80,7 @@ class BulkDataPointEditPageController {
         
         this.bulkTask = new this.maPoint.bulk({
             action: 'UPDATE',
-            body: {
-                setPermission: this.setPermission
-            },
+            body: this.updateBody,
             requests: this.selectedPoints.map(pt => {
                 return {xid: pt.xid};
             })
@@ -51,7 +90,7 @@ class BulkDataPointEditPageController {
             this.results = resource.result.responses;
             this.results.forEach((result, i) => {
                 if (result.body) {
-                    angular.copy(result.body, this.points[i]);
+                    angular.copy(result.body, this.selectedPoints[i]);
                 }
             });
         }, error => {
@@ -62,29 +101,48 @@ class BulkDataPointEditPageController {
             this.results = resource.result.responses;
             this.results.forEach((result, i) => {
                 if (result.body) {
-                    angular.copy(result.body, this.points[i]);
+                    angular.copy(result.body, this.selectedPoints[i]);
                 }
             });
         }).finally(() => {
             delete this.bulkTaskPromise;
         });
     }
-    
+
     watchListChanged() {
         if (!this.watchList) return;
-        
-        this.selectedPoints = [];
-        this.selectAll = false;
-        this.selectAllIndeterminate = false;
-
+        this.reset();
         this.pointsPromise = this.watchList.getPoints().then(points => {
             this.points = points;
+            
+            const seenTagKeys = {};
+            this.points.forEach(pt => {
+                Object.keys(pt.tags).forEach(key => {
+                    seenTagKeys[key] = true;
+                });
+            });
+            
+            this.selectedTags = Object.keys(seenTagKeys).map(tagKey => {
+                return this.availableTagsByKey[tagKey];
+            });
+            
         }, () => {
             // TODO toast error
             this.points = [];
         }).finally(() => {
             delete this.pointsPromise;
         });
+    }
+    
+    addTagColumn() {
+        const tagKey = this.tagColumnToAdd;
+        delete this.tagColumnToAdd;
+
+        const newOption = this.addTagToAvailable(tagKey);
+        if (!newOption) {
+            return;
+        }
+        this.selectedTags.push(newOption);
     }
     
     selectedPointsChanged() {
@@ -113,6 +171,23 @@ class BulkDataPointEditPageController {
         }
         
         this.selectAllIndeterminate = false;
+    }
+    
+    setTag(tag) {
+        if (!this.updateBody) this.updateBody = {};
+        if (!this.updateBody.tags) this.updateBody.tags = {};
+        this.updateBody.tags[tag.name] = this.tagVal;
+    }
+    
+    removeTag(tag) {
+        if (!this.updateBody) this.updateBody = {};
+        if (!this.updateBody.tags) this.updateBody.tags = {};
+        delete this.updateBody.tags[tag.name];
+    }
+    
+    setSetPermission() {
+        if (!this.updateBody) this.updateBody = {};
+        this.updateBody.setPermission = this.setPermission;
     }
 }
 
