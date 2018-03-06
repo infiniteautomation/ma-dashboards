a7560c1fb91e9f98d1eded9ac8322a675cbcceb8
Jared Wiltshire <jazdw@users.noreply.github.com>
2018-03-06 16:22:14 -0700

    Remove import

--- UI/web/ngMango/ngMango.js
+++ UI/web/ngMango/ngMango.js
@@ -113,7 +113,6 @@ import slideUp from './animations/slideUp';
 import angular from 'angular';import moment from 'moment-timezone';
 import AmCharts from 'amcharts/amcharts';
 
-import 'angular/angular-csp.css';
 import './ngMango.css';
 
 /**
