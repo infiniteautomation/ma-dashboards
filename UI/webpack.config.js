724fb8efe52293963a29b48d084ebc8c63d40822
Jared Wiltshire <jazdw@users.noreply.github.com>
2018-03-13 17:49:47 -0600

    Add shims, amcharts export plugin works

--- UI/webpack.config.js
+++ UI/webpack.config.js
@@ -74,14 +74,44 @@ module.exports = readPom(__dirname).then(pom => {
                     loader: 'imports-loader?define=>false'
                 },
                 {
-                    test: /amcharts/,
-                    include: /\.js$/,
-                    use: ['exports-loader?window.AmCharts', 'imports-loader?AmCharts=amcharts/amcharts']
+                    include: path.resolve(__dirname, 'vendor/amcharts/amcharts.js'),
+                    use: ['exports-loader?window.AmCharts']
                 },
                 {
-                    test: /md-color-picker/,
-                    include: /\.js$/,
-                    use: ['imports-loader?tinycolor=tinycolor2']
+                    test: /\.js$/,
+                    include: path.resolve(__dirname, 'vendor/amcharts'),
+                    exclude: [
+                        path.resolve(__dirname, 'vendor/amcharts/amcharts.js'),
+                        path.resolve(__dirname, 'vendor/amcharts/plugins/export/libs')
+                    ],
+                    use: ['imports-loader?AmCharts=amcharts/amcharts', 'exports-loader?window.AmCharts']
+                },
+                {
+                    include: path.resolve(__dirname, 'vendor/amcharts/plugins/export/export.js'),
+                    use: [
+                        {
+                            loader: 'imports-loader',
+                            options: {
+                                'windowTemp': '>window',
+                                'fileSaver': 'file-saver',
+                                'vfs_fonts': 'pdfmake/build/vfs_fonts',
+                                'fabricModule': 'amcharts/plugins/export/libs/fabric.js/fabric',
+                                'windowTemp.saveAs': '>fileSaver.saveAs',
+                                'windowTemp.fabric': '>fabricModule.fabric',
+                                'windowTemp.pdfMake': 'pdfmake/build/pdfmake',
+                                'windowTemp.pdfMake.vfs': '>vfs_fonts.pdfMake.vfs',
+                                'windowTemp.XLSX': 'xlsx/dist/xlsx'
+                            }
+                        }
+                    ]
+                },
+                {
+                    include: path.resolve(__dirname, 'node_modules/xlsx/dist/xlsx.js'),
+                    use: [{loader:'imports-loader', options: {JSZipSync: 'xlsx/dist/jszip'}}]
+                },
+                {
+                    include: path.resolve(__dirname, 'vendor/amcharts/plugins/export/libs/fabric.js/fabric.js'),
+                    use: [{loader:'imports-loader', options: {require: '>undefined'}}]
                 },
                 {
                     test: /rql/,
@@ -99,7 +129,16 @@ module.exports = readPom(__dirname).then(pom => {
                 },
                 {
                     test: /angular\.js$/,
-                    use: ['imports-loader?windowTemp=>window&windowTemp.jQuery=jquery']
+                    use: ['imports-loader?windowTemp=>window,windowTemp.jQuery=jquery']
+                },
+                {
+                    test: /angular-.+?\.js$/,
+                    use: ['imports-loader?angular']
+                },
+                {
+                    test: /angular-ui-sortable/,
+                    include: /\.js$/,
+                    use: ['imports-loader?jqueryUiTouchPunch=jquery-ui-touch-punch-c']
                 },
                 {
                     test: /angular-ui-ace/,
@@ -110,6 +149,36 @@ module.exports = readPom(__dirname).then(pom => {
                     test: /ace-builds/,
                     include: /\.js$/,
                     use: ['imports-loader?requirejs=>window.requirejs,require=>window.require,define=>window.define']
+                },
+                {
+                    test: /jquery-ui/,
+                    include: /\.js$/,
+                    use: ['imports-loader?jquery']
+                },
+                {
+                    test: /jquery-ui-touch-punch/,
+                    include: /\.js$/,
+                    use: ['imports-loader?jqueryUi=jquery-ui']
+                },
+                {
+                    test: /mdPickers/,
+                    include: /\.js$/,
+                    use: ['imports-loader?moment=moment-timezone,angular,angularMaterial=angular-material']
+                },
+                {
+                    test: /angular-material-data-table/,
+                    include: /\.js$/,
+                    use: ['imports-loader?angular,angularMaterial=angular-material']
+                },
+                {
+                    test: /md-color-picker/,
+                    include: /\.js$/,
+                    use: ['imports-loader?angular,angularMaterial=angular-material,tinycolor=tinycolor2']
+                },
+                {
+                    test: /ng-map/,
+                    include: /\.js$/,
+                    use: ['imports-loader?angular']
                 }
             ]
         },
