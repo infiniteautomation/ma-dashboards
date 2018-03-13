2b7085604fa4db19312634b18baf67a89cfdad93
Jared Wiltshire <jazdw@users.noreply.github.com>
2018-03-13 17:49:22 -0600

    Dont write package.json unless changed

--- UI/updatePackage.js
+++ UI/updatePackage.js
@@ -8,12 +8,19 @@ const fs = require('fs');
 const packageJson = require('./package.json');
 
 module.exports = function updatePackage(pom) {
+    const oldContents = JSON.stringify(packageJson, null, 2);
+    
     packageJson.name = pom.project.name[0];
     packageJson.version = pom.project.version[0];
     packageJson.description = pom.project.description[0];
-    
+
+    const newContents = JSON.stringify(packageJson, null, 2);
+    if (newContents === oldContents) {
+        return Promise.resolve(packageJson);
+    }
+
     return new Promise((resolve, reject) => {
-        fs.writeFile(path.join(__dirname, 'package.json'), JSON.stringify(packageJson, null, 2), error => {
+        fs.writeFile(path.join(__dirname, 'package.json'), newContents, error => {
             if (error) {
                 reject(error);
             } else {
