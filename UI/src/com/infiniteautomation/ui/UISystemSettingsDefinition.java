770b8b4fa0a111688a25ff1bd522476574a1f32a
Jared Wiltshire <jazdw@users.noreply.github.com>
2018-08-16 15:30:41 -0600

    Add system setting default

--- UI/src/com/infiniteautomation/ui/UISystemSettingsDefinition.java
+++ UI/src/com/infiniteautomation/ui/UISystemSettingsDefinition.java
@@ -14,60 +14,61 @@ import com.serotonin.m2m2.module.SystemSettingsDefinition;
  */
 public class UISystemSettingsDefinition extends SystemSettingsDefinition {
 
-	/* (non-Javadoc)
-	 * @see com.serotonin.m2m2.module.SystemSettingsDefinition#getDescriptionKey()
-	 */
-	@Override
-	public String getDescriptionKey() {
-		return "ui.settings";
-	}
+    /* (non-Javadoc)
+     * @see com.serotonin.m2m2.module.SystemSettingsDefinition#getDescriptionKey()
+     */
+    @Override
+    public String getDescriptionKey() {
+        return "ui.settings";
+    }
 
-	/* (non-Javadoc)
-	 * @see com.serotonin.m2m2.module.SystemSettingsDefinition#getSectionJspPath()
-	 */
-	@Override
-	public String getSectionJspPath() {
-		return null;
-	}
+    /* (non-Javadoc)
+     * @see com.serotonin.m2m2.module.SystemSettingsDefinition#getSectionJspPath()
+     */
+    @Override
+    public String getSectionJspPath() {
+        return null;
+    }
 
-	/* (non-Javadoc)
-	 * @see com.serotonin.m2m2.module.SystemSettingsDefinition#getDefaultValues()
-	 */
-	@Override
-	public Map<String, Object> getDefaultValues() {
-		Map<String, Object> defaults = new HashMap<String, Object>();
-		defaults.put(UICommon.UI_FIRST_USER_LOGIN_PAGE, UICommon.DEFAULT_UI_FIRST_USER_LOGIN_PAGE);
-		defaults.put(UICommon.UI_LOGGED_IN_PAGE, UICommon.DEFAULT_UI_LOGGED_IN_PAGE);
+    /* (non-Javadoc)
+     * @see com.serotonin.m2m2.module.SystemSettingsDefinition#getDefaultValues()
+     */
+    @Override
+    public Map<String, Object> getDefaultValues() {
+        Map<String, Object> defaults = new HashMap<String, Object>();
+        defaults.put(UICommon.UI_FIRST_USER_LOGIN_PAGE, UICommon.DEFAULT_UI_FIRST_USER_LOGIN_PAGE);
+        defaults.put(UICommon.UI_PASSWORD_RESET_PAGE, UICommon.DEFAULT_UI_PASSWORD_RESET_PAGE);
+        defaults.put(UICommon.UI_LOGGED_IN_PAGE, UICommon.DEFAULT_UI_LOGGED_IN_PAGE);
         defaults.put(UICommon.UI_LOGGED_IN_PAGE_PRE_HOME, UICommon.DEFAULT_UI_LOGGED_IN_PAGE_PRE_HOME);
-		defaults.put(UICommon.UI_LOGIN_PAGE, UICommon.DEFAULT_UI_LOGIN_PAGE);
-		defaults.put(UICommon.UI_UNAUTHORIZED_PAGE, UICommon.DEFAULT_UI_UNAUTHORIZED_PAGE);
-		defaults.put(UICommon.UI_NOT_FOUND_PAGE, UICommon.DEFAULT_UI_NOT_FOUND_PAGE);
-		defaults.put(UICommon.UI_ERROR_PAGE, UICommon.DEFAULT_UI_ERROR_PAGE);
-		return defaults;
-	}
+        defaults.put(UICommon.UI_LOGIN_PAGE, UICommon.DEFAULT_UI_LOGIN_PAGE);
+        defaults.put(UICommon.UI_UNAUTHORIZED_PAGE, UICommon.DEFAULT_UI_UNAUTHORIZED_PAGE);
+        defaults.put(UICommon.UI_NOT_FOUND_PAGE, UICommon.DEFAULT_UI_NOT_FOUND_PAGE);
+        defaults.put(UICommon.UI_ERROR_PAGE, UICommon.DEFAULT_UI_ERROR_PAGE);
+        return defaults;
+    }
 
-	/* (non-Javadoc)
-	 * @see com.serotonin.m2m2.module.SystemSettingsDefinition#convertToValueFromCode(java.lang.String, java.lang.String)
-	 */
-	@Override
-	public Integer convertToValueFromCode(String key, String code) {
-		return null;
-	}
+    /* (non-Javadoc)
+     * @see com.serotonin.m2m2.module.SystemSettingsDefinition#convertToValueFromCode(java.lang.String, java.lang.String)
+     */
+    @Override
+    public Integer convertToValueFromCode(String key, String code) {
+        return null;
+    }
 
-	/* (non-Javadoc)
-	 * @see com.serotonin.m2m2.module.SystemSettingsDefinition#convertToCodeFromValue(java.lang.String, java.lang.Integer)
-	 */
-	@Override
-	public String convertToCodeFromValue(String key, Integer value) {
-		return null;
-	}
+    /* (non-Javadoc)
+     * @see com.serotonin.m2m2.module.SystemSettingsDefinition#convertToCodeFromValue(java.lang.String, java.lang.Integer)
+     */
+    @Override
+    public String convertToCodeFromValue(String key, Integer value) {
+        return null;
+    }
 
-	/* (non-Javadoc)
-	 * @see com.serotonin.m2m2.module.SystemSettingsDefinition#validateSettings(java.util.Map, com.serotonin.m2m2.i18n.ProcessResult)
-	 */
-	@Override
-	public void validateSettings(Map<String, Object> settings, ProcessResult response) {
-		// TODO Not Sure How to Validate These?
-	}
+    /* (non-Javadoc)
+     * @see com.serotonin.m2m2.module.SystemSettingsDefinition#validateSettings(java.util.Map, com.serotonin.m2m2.i18n.ProcessResult)
+     */
+    @Override
+    public void validateSettings(Map<String, Object> settings, ProcessResult response) {
+        // TODO Not Sure How to Validate These?
+    }
 
 }
