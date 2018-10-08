350941d7521da1f6744e942b346710a15a3782a2
Jared Wiltshire <jazdw@users.noreply.github.com>
2018-10-08 17:17:09 -0600

    Update mango-module-tools

--- mango-module-tools/module.config.js
+++ mango-module-tools/module.config.js
@@ -9,100 +9,112 @@ const CopyWebpackPlugin = require('copy-webpack-plugin');
 const readPom = require('./readPom');
 const updatePackage = require('./updatePackage');
 
-module.exports = readPom().then(pom => {
-    return updatePackage(pom);
-}).then(packageJson => {
-    const moduleName = packageJson.com_infiniteautomation.moduleName;
-    return {
-        entry: {
-            [moduleName]: `./web-src/${moduleName}.js`
-        },
-        module: {
-            rules: [
-                {
-                    test: /\.html$/,
-                    use: [{
-                        loader: 'html-loader',
-                        options: {
-                        }
-                    }]
-                },
-                {
-                    test: /\.css$/,
-                    use: [
-                        {
-                            loader: 'style-loader',
+module.exports = (configOptions = {}) => {
+    const moduleRoot = configOptions.moduleRoot || path.resolve('.');
+    
+    return readPom(moduleRoot).then(pom => {
+        return updatePackage(pom, moduleRoot);
+    }).then(packageJson => {
+        const moduleName = packageJson.com_infiniteautomation.moduleName;
+        
+        const webPackConfig = {
+            entry: {
+                [moduleName]: `./web-src/${moduleName}.js`
+            },
+            module: {
+                rules: [
+                    {
+                        test: /\.html$/,
+                        use: [{
+                            loader: 'html-loader',
                             options: {
-                                insertAt: {
-                                    before: 'meta[name="user-styles-after-here"]'
-                                }
                             }
-                        },
-                        {
-                            loader: 'css-loader'
-                        }
-                    ]
-                },
-                {
-                    test: /\.(png|svg|jpg|jpeg|gif)$/,
-                    use: [{
-                        loader: 'file-loader',
-                        options: {
-                            name: 'images/[name].[ext]?v=[hash]'
-                        }
-                    }]
-                },
-                {
-                    test: /\.(woff|woff2|eot|ttf|otf)$/,
-                    use: [{
-                        loader: 'file-loader',
-                        options: {
-                            name: 'fonts/[name].[ext]?v=[hash]'
-                        }
-                    }]
-                },
-                {
-                    test: /\.(txt|csv)$/,
-                    use: [{
-                        loader: 'raw-loader'
-                    }]
-                }
-            ]
-        },
-        plugins: [
-            new CleanWebpackPlugin(['web']),
-            new CopyWebpackPlugin([{
-                context: 'web-src/static',
-                from: '**/*'
-            }])
-        ],
-        output: {
-            filename: '[name].js?v=[chunkhash]',
-            path: path.resolve('web'),
-            publicPath: `/modules/${moduleName}/web/`,
-            libraryTarget: 'umd',
-            libraryExport: 'default',
-            library: moduleName
-        },
-        externals: {
-            'angular': 'angular',
-            'cldrjs': 'cldrjs',
-            'cldr-data': 'cldr-data',
-            'file-saver': 'file-saver',
-            'globalize': 'globalize',
-            'ipaddr.js': 'ipaddr.js',
-            'jquery': 'jquery',
-            'js-sha512': 'sha512',
-            'jszip': 'jszip',
-            'mathjs': 'mathjs',
-            'moment': 'moment',
-            'moment-timezone': 'moment-timezone',
-            'papaparse': 'papaparse',
-            'pdfmake': 'pdfmake',
-            'plotly.js': 'plotly.js',
-            'stacktrace-js': 'stacktrace-js',
-            'tinycolor2': 'tinycolor2',
-            'xlsx': 'xlsx'
-        }
-    };
-});
+                        }]
+                    },
+                    {
+                        test: /\.css$/,
+                        use: [
+                            {
+                                loader: 'style-loader',
+                                options: {
+                                    insertAt: {
+                                        before: 'meta[name="user-styles-after-here"]'
+                                    }
+                                }
+                            },
+                            {
+                                loader: 'css-loader'
+                            }
+                        ]
+                    },
+                    {
+                        test: /\.(png|svg|jpg|jpeg|gif)$/,
+                        use: [{
+                            loader: 'file-loader',
+                            options: {
+                                name: 'images/[name].[ext]?v=[hash]'
+                            }
+                        }]
+                    },
+                    {
+                        test: /\.(woff|woff2|eot|ttf|otf)$/,
+                        use: [{
+                            loader: 'file-loader',
+                            options: {
+                                name: 'fonts/[name].[ext]?v=[hash]'
+                            }
+                        }]
+                    },
+                    {
+                        test: /\.(txt|csv)$/,
+                        use: [{
+                            loader: 'raw-loader'
+                        }]
+                    }
+                ]
+            },
+            optimization: {
+                splitChunks: false
+            },
+            plugins: [
+                new CleanWebpackPlugin(['web/angular'], {
+                    root: moduleRoot
+                }),
+                new CopyWebpackPlugin([{
+                    context: 'web-src/static',
+                    from: '**/*'
+                }])
+            ],
+            output: {
+                filename: '[name].js?v=[chunkhash]',
+                path: path.resolve('web', 'angular'),
+                publicPath: `/modules/${moduleName}/web/angular/`,
+                libraryTarget: 'umd',
+                libraryExport: 'default',
+                library: moduleName
+            },
+            externals: {
+                'angular': 'angular',
+                'cldrjs': 'cldrjs',
+                'cldr-data': 'cldr-data',
+                'file-saver': 'file-saver',
+                'globalize': 'globalize',
+                'ipaddr.js': 'ipaddr.js',
+                'jquery': 'jquery',
+                'js-sha512': 'sha512',
+                'jszip': 'jszip',
+                'mathjs': 'mathjs',
+                'moment': 'moment',
+                'moment-timezone': 'moment-timezone',
+                'papaparse': 'papaparse',
+                'pdfmake': 'pdfmake',
+                'plotly.js': 'plotly.js',
+                'stacktrace-js': 'stacktrace-js',
+                'tinycolor2': 'tinycolor2',
+                'xlsx': 'xlsx'
+            }
+        };
+        
+        return webPackConfig;
+    });
+};
