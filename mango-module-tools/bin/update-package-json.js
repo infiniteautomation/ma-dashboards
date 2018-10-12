07bd95d7e9fdcc6d14257941fef2a43d3821c345
Jared Wiltshire <jazdw@users.noreply.github.com>
2018-10-12 15:07:35 -0600

    Fix requires

--- mango-module-tools/bin/update-package-json.js
+++ mango-module-tools/bin/update-package-json.js
@@ -1,8 +1,8 @@
 #!/usr/bin/env node
 
 const [,, ...args] = process.argv;
-const readPom = require('./readPom');
-const updatePackage = require('./updatePackage');
+const readPom = require('../readPom');
+const updatePackage = require('../updatePackage');
 const path = require('path');
 
 const rootDirectory = path.resolve('.');
