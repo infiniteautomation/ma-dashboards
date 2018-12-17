f507d8f71e98cc4cda461a705a9de4b8cad46a90
Luis Güette <guetteluis@gmail.com>
2018-12-17 19:59:56 -0400

    add save, delete and cancel buttons

--- UI/web-src/ngMango/components/virtualSerialPort/virtualSerialPortSetup.js
+++ UI/web-src/ngMango/components/virtualSerialPort/virtualSerialPortSetup.js
@@ -6,15 +6,17 @@
 import componentTemplate from './virtualSerialPortSetup.html';
 import angular from 'angular';
 
-const $inject = Object.freeze(['$scope']);
+const $inject = Object.freeze(['$scope', 'maDialogHelper', 'maVirtualSerialPort']);
 
 class VirtualSerialPortSetupController {
 
     static get $inject() { return $inject; }
     static get $$ngIsClass() { return true; }
     
-    constructor($scope) {
+    constructor($scope, maDialogHelper, maVirtualSerialPort) {
         this.$scope = $scope;
+        this.maDialogHelper = maDialogHelper;
+        this.maVirtualSerialPort = maVirtualSerialPort;
     }
 
     $onInit() {
@@ -33,13 +35,41 @@ class VirtualSerialPortSetupController {
     }
 
     save() {  
-
         this.form.$setSubmitted();
+
+        this.virtualSerialPort.save().then(response => {
+            this.maDialogHelper.toastOptions({
+                textTr: ['systemSettings.comm.virtual.serialPortSaved'],
+                hideDelay: 5000
+            });
+        }, error => {
+            this.maDialogHelper.toastOptions({
+                textTr: ['ui.app.virtualSerialPort.notSaved', error.mangoStatusText],
+                classes: 'md-warn',
+                hideDelay: 5000
+            });
+        });
+        
+    }
+
+    delete() {  
+        this.virtualSerialPort.delete().then(response => {
+            this.maDialogHelper.toastOptions({
+                textTr: ['systemSettings.comm.virtual.serialPortRemoved'],
+                hideDelay: 5000
+            });
+        }, error => {
+            this.maDialogHelper.toastOptions({
+                textTr: ['ui.app.virtualSerialPort.notRemoved', error.mangoStatusText],
+                classes: 'md-warn',
+                hideDelay: 5000
+            });
+        });
         
     }
 
     cancel(event) {
-        this.list = new this.maMailingList();
+        this.virtualSerialPort = new this.maVirtualSerialPort();
         this.setViewValue();
         this.render();
     }
