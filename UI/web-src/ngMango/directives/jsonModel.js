e978cdb776363fbf69e9593cf4a8d37ae7078677
Jared Wiltshire <jazdw@users.noreply.github.com>
2019-01-15 11:08:50 -0700

    Display JSON parsing errors on JSON editor page, dont allow save

--- UI/web-src/ngMango/directives/jsonModel.js
+++ UI/web-src/ngMango/directives/jsonModel.js
@@ -15,14 +15,24 @@ class JsonModelController {
     
     $onInit() {
         this.ngModelCtrl.$parsers.push(value => {
+            this.ngModelCtrl.$setValidity('jsonParseError', true);
+            delete this.ngModelCtrl.jsonParseError;
+            
             if (typeof value !== 'string' || value === '')
                 return value;
-            return angular.fromJson(value);
+            
+            try {
+                return angular.fromJson(value);
+            } catch (e) {
+                this.ngModelCtrl.$setValidity('jsonParseError', false);
+                this.ngModelCtrl.jsonParseError = '' + e;
+            }
         });
         
         this.ngModelCtrl.$formatters.push(value => {
             if (value === undefined)
                 return value;
+            
             return angular.toJson(value, this.pretty);
         });
     }
@@ -43,5 +53,3 @@ const jsonModel = function jsonModel() {
 };
 
 export default jsonModel;
-
-
