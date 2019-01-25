6e9a4994e06d0aa092abd7de903f179fbeaffcd4
Jared Wiltshire <jazdw@users.noreply.github.com>
2019-01-25 12:08:21 -0700

    Override methods to supply event type name

--- UI/web-src/ngMango/services/EventType.js
+++ UI/web-src/ngMango/services/EventType.js
@@ -3,8 +3,8 @@
  * @author Jared Wiltshire
  */
 
-eventTypeFactory.$inject = ['maRestResource'];
-function eventTypeFactory(RestResource) {
+eventTypeFactory.$inject = ['maRestResource', 'maRqlBuilder'];
+function eventTypeFactory(RestResource, RqlBuilder) {
 
     const eventTypeBaseUrl = '/rest/v2/event-types';
     
@@ -26,6 +26,47 @@ function eventTypeFactory(RestResource) {
         
         static uniqueId(type) {
             return `${type.eventType}_${type.subType}_${type.referenceId1}_${type.referenceId2}`;
+        }
+        
+        static list(name, opts = {}) {
+            return this.query(name, null, opts);
+        }
+        
+        static buildQuery(name) {
+            const builder = new RqlBuilder();
+            builder.queryFunction = (queryObj, opts) => {
+                return this.query(name, queryObj, opts);
+            };
+            return builder;
+        }
+
+        static query(name, queryObject, opts = {}) {
+            const params = {};
+            
+            if (queryObject) {
+                const rqlQuery = queryObject.toString();
+                if (rqlQuery) {
+                    params.rqlQuery = rqlQuery;
+                }
+            }
+            
+            const nameEncoded = this.encodeUriSegment(name);
+            
+            return this.http({
+                url: `${this.baseUrl}/${nameEncoded}`,
+                method: 'GET',
+                params: params
+            }, opts).then(response => {
+                if (opts.responseType != null) {
+                    return response.data;
+                }
+                
+                const items = response.data.items.map(item => {
+                    return new this(item);
+                });
+                items.$total = response.data.total;
+                return items;
+            });
         }
     }
     
