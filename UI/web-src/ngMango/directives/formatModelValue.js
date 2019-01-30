8040c2d2050ce6ad6a150528534f85ff72579579
Jared Wiltshire <jazdw@users.noreply.github.com>
2019-01-30 10:54:12 -0700

    Work around for md-input-container appearing to have value when empty

--- UI/web-src/ngMango/directives/formatModelValue.js
+++ UI/web-src/ngMango/directives/formatModelValue.js
@@ -6,14 +6,20 @@
 formatModelValue.$inject = ['$parse'];
 function formatModelValue($parse) {
     return {
-        require: 'ngModel',
+        require: ['ngModel', '^?mdInputContainer'],
         restrict: 'A',
-        link: function($scope, $element, $attrs, ngModel) {
+        link: function($scope, $element, $attrs, [ngModel, containerCtrl]) {
             if (!$attrs.maFormatModelValue) return;
             
             const expression = $parse($attrs.maFormatModelValue);
             ngModel.$formatters.push(function(value) {
-                return expression($scope, {$modelValue: value});
+                const result = expression($scope, {$modelValue: value});
+                if (containerCtrl) {
+                    // the mdInputContainer adds a formatter which runs before this one which sets the
+                    // .md-input-has-value class, work around by setting it again
+                    containerCtrl.setHasValue(!ngModel.$isEmpty(result));
+                }
+                return result;
             });
         }
     };
