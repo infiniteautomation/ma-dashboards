fa88c2526d0ae93a68b1a4c1de6a890d1c8feb2b
Jared Wiltshire <jazdw@users.noreply.github.com>
2019-02-06 13:27:48 -0700

    Add empty container workaround to ma-array-input

--- UI/web-src/ngMango/directives/arrayInput.js
+++ UI/web-src/ngMango/directives/arrayInput.js
@@ -6,9 +6,9 @@
 arrayInput.$inject = [];
 function arrayInput() {
     return {
-        require: 'ngModel',
+        require: ['ngModel', '^?mdInputContainer'],
         restrict: 'A',
-        link: function($scope, $element, $attrs, ngModel) {
+        link: function($scope, $element, $attrs, [ngModel, containerCtrl]) {
             ngModel.$parsers.push(function toArray(viewValue) {
                 if (typeof viewValue !== 'string') return viewValue;
                 if (!viewValue.trim().length) return [];
@@ -17,7 +17,13 @@ function arrayInput() {
             
             ngModel.$formatters.push(function fromArray(modelValue) {
                 if (!Array.isArray(modelValue)) return modelValue;
-                return modelValue.map(p => p.trim()).join($attrs.arrayDelimiter || ', ');
+                const result = modelValue.map(p => p.trim()).join($attrs.arrayDelimiter || ', ');
+                if (containerCtrl) {
+                    // the mdInputContainer adds a formatter which runs before this one which sets the
+                    // .md-input-has-value class, work around by setting it again
+                    containerCtrl.setHasValue(!ngModel.$isEmpty(result));
+                }
+                return result;
             });
         }
     };
