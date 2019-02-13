a8e4acddf1103aac5557027134d7e8c57c185221
Jared Wiltshire <jazdw@users.noreply.github.com>
2019-02-13 11:16:03 -0700

    Allow excluding from form only if parent is excluded

--- UI/web-src/ngMango/directives/formExclude.js
+++ UI/web-src/ngMango/directives/formExclude.js
@@ -3,8 +3,11 @@
  * @author Jared Wiltshire
  */
 
-formExclude.$inject = [];
-function formExclude() {
+formExclude.$inject = ['$document'];
+function formExclude($document) {
+    const excludeAttr = 'ma-form-exclude';
+    const excludeParentValue = 'parent';
+    
     return {
         require: {
             modelCtrl: '?ngModel',
@@ -14,7 +17,18 @@ function formExclude() {
         link: function($scope, $element, $attrs, ctrls) {
             const modelCtrl = ctrls.modelCtrl;
             const formCtrl = ctrls.formCtrl;
-            
+            const thisElement = $element[0];
+
+            if ($attrs.maFormExclude === excludeParentValue) {
+                const formExcludes = $document[0].querySelectorAll(`[${excludeAttr}]`);
+                const parentExcluded = Array.prototype.some.call(formExcludes, el => {
+                    return el !== thisElement && el.contains(thisElement) && el.getAttribute(excludeAttr) !== excludeParentValue;
+                });
+                if (!parentExcluded) {
+                    return;
+                }
+            }
+
             if (modelCtrl) {
                 modelCtrl.$$parentForm.$removeControl(modelCtrl);
             }
