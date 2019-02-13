61870d47fc3da6a5ce75ee1d0ff51f413e6df3da
Jared Wiltshire <jazdw@users.noreply.github.com>
2019-02-13 12:48:36 -0700

    Add ma-event-handler-check-list component and ma-array-model directive

---
+++ UI/web-src/ngMango/components/eventHandlerCheckList/eventHandlerCheckList.js
@@ -0,0 +1,91 @@
+/**
+ * @copyright 2018 {@link http://infiniteautomation.com|Infinite Automation Systems, Inc.} All rights reserved.
+ * @author Jared Wiltshire
+ */
+
+import eventHandlerCheckListTemplate from './eventHandlerCheckList.html';
+
+/**
+ * @ngdoc directive
+ * @name ngMango.directive:maEventHandlerCheckList
+ * @restrict E
+ * @description Displays a list of event handlers
+ */
+
+class EventHandlerCheckListController {
+    static get $$ngIsClass() { return true; }
+    static get $inject() { return ['maEventHandler', '$scope']; }
+    
+    constructor(maEventHandler, $scope) {
+        this.maEventHandler = maEventHandler;
+        this.$scope = $scope;
+    }
+    
+    $onInit() {
+        this.ngModelCtrl.$render = () => this.render();
+        
+        this.doQuery();
+        
+        this.maEventHandler.keepUpdated({
+            items: () => this.eventHandlers,
+            scope: this.$scope
+        });
+        
+        this.selected = new Map();
+    }
+
+    $onChanges(changes) {
+    }
+    
+    doQuery() {
+        const queryBuilder = this.maEventHandler.buildQuery();
+        queryBuilder.limit(10000);
+        return queryBuilder.query().then(eventHandlers => {
+            return (this.eventHandlers = eventHandlers);
+        });
+    }
+    
+    setViewValue() {
+        this.ngModelCtrl.$setViewValue(Array.from(this.selected.values()));
+    }
+    
+    render() {
+        this.selected.clear();
+        const selected = this.ngModelCtrl.$viewValue;
+        if (Array.isArray(selected)) {
+            selected.forEach(eh => this.selected.set(eh.xid, eh));
+        }
+    }
+
+    selectedGetterSetter(eventHandler) {
+        return value => {
+            const xid = eventHandler.xid;
+            
+            if (value === undefined) {
+                return this.selected.has(xid);
+            }
+            
+            if (value) {
+                this.selected.set(xid, eventHandler);
+            } else {
+                this.selected.delete(xid);
+            }
+            
+            this.setViewValue();
+        };
+    }
+}
+
+export default {
+    template: eventHandlerCheckListTemplate,
+    controller: EventHandlerCheckListController,
+    bindings: {
+    },
+    require: {
+        ngModelCtrl: 'ngModel'
+    },
+    designerInfo: {
+        translation: 'ui.components.eventHandlerCheckList',
+        icon: 'assignment_turned_in'
+    }
+};
