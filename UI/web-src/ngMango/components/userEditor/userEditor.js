7ab21c8c29f0f743e68d3117b2319c4f3777901f
Jared Wiltshire <jazdw@users.noreply.github.com>
2019-03-01 11:29:06 -0700

    Convert to ES6 class, throb send email button, use maDialogHelper
    Show message about fixing errors on form

--- UI/web-src/ngMango/components/userEditor/userEditor.js
+++ UI/web-src/ngMango/components/userEditor/userEditor.js
@@ -8,45 +8,50 @@ import userEditorTemplate from './userEditor.html';
 import './userEditor.css';
 import moment from 'moment-timezone';
 
-UserEditorController.$inject = ['maUser', '$http', '$mdDialog', 'maTranslate', '$mdToast', 'maLocales', '$window', '$injector'];
-function UserEditorController(User, $http, $mdDialog, Translate, $mdToast, maLocales, $window, $injector) {
-    this.User = User;
-    this.$http = $http;
-    this.timezones = moment.tz.names();
-    this.$mdDialog = $mdDialog;
-    this.Translate = Translate;
-    this.$mdToast = $mdToast;
-    this.$window = $window;
-    this.$state = $injector.has('$state') && $injector.get('$state');
+class UserEditorController {
+    static get $$ngIsClass() { return true; }
+    static get $inject() { return ['maUser', '$http', '$mdDialog', 'maTranslate', 'maLocales', '$window', '$injector', 'maDialogHelper']; }
     
-    maLocales.get().then(function(locales) {
-        this.locales = locales;
-    }.bind(this));
-}
-
-UserEditorController.prototype.$onChanges = function(changes) {
-    if (changes.originalUser && this.originalUser) {
-        this.user = angular.copy(this.originalUser);
-        this.resetForm();
+    constructor(User, $http, $mdDialog, Translate, maLocales, $window, $injector, maDialogHelper) {
+        this.User = User;
+        this.$http = $http;
+        this.timezones = moment.tz.names();
+        this.$mdDialog = $mdDialog;
+        this.Translate = Translate;
+        this.$window = $window;
+        this.$state = $injector.has('$state') && $injector.get('$state');
+        this.maDialogHelper = maDialogHelper;
+        
+        maLocales.get().then(locales => {
+            this.locales = locales;
+        });
     }
-};
-
-UserEditorController.prototype.hashRegExp = /^\{(.*?)\}(.*)$/;
-
-UserEditorController.prototype.resetForm = function() {
-    this.password = '';
-    this.confirmPassword = '';
     
-    delete this.validationMessages;
-    
-    if (this.userForm) {
-        this.userForm.$setPristine();
-        this.userForm.$setUntouched();
+    $onChanges(changes) {
+        if (changes.originalUser && this.originalUser) {
+            this.user = angular.copy(this.originalUser);
+            this.resetForm();
+        }
     }
-};
 
-UserEditorController.prototype.save = function() {
-    if (this.userForm.$valid) {
+    resetForm() {
+        this.password = '';
+        this.confirmPassword = '';
+        
+        delete this.validationMessages;
+        
+        if (this.userForm) {
+            this.userForm.$setPristine();
+            this.userForm.$setUntouched();
+        }
+    }
+    
+    save() {
+        if (!this.userForm.$valid) {
+            this.maDialogHelper.errorToast('ui.components.fixErrorsOnForm');
+            return;
+        }
+        
         if (this.password) {
             this.user.password = this.password;
         }
@@ -59,135 +64,80 @@ UserEditorController.prototype.save = function() {
             if (previous.username === this.User.current.username) {
                 this.User.current = this.originalUser;
             }
-            
-            const toast = this.$mdToast.simple()
-                .textContent(this.Translate.trSync('ui.components.userSaved', user.username))
-                .action(this.Translate.trSync('common.ok'))
-                .highlightAction(true)
-                .position('bottom center')
-                .hideDelay(10000);
-            this.$mdToast.show(toast);
+    
+            this.maDialogHelper.toast(['ui.components.userSaved', user.username]);
             
             this.onSave({$user: this.originalUser, $previous: previous});
             this.resetForm();
-        }, response => {
-            if (response.data && response.data.validationMessages) {
-                this.validationMessages = response.data.validationMessages;
+        }, error => {
+            if (error.data && error.data.validationMessages) {
+                this.validationMessages = error.data.validationMessages;
             }
-            
-            let errorText = this.Translate.trSync('ui.components.errorSavingUser', this.user.username, response.mangoStatusText);
-            
-            const toast = this.$mdToast.simple()
-                .textContent(errorText)
-                .action(this.Translate.trSync('common.ok'))
-                .highlightAction(true)
-                .toastClass('md-warn')
-                .position('bottom center')
-                .hideDelay(10000);
-            this.$mdToast.show(toast);
+    
+            this.maDialogHelper.errorToast(['ui.components.errorSavingUser', this.user.username, error.mangoStatusText]);
         });
     }
-};
-
-UserEditorController.prototype.revert = function() {
-    this.user = angular.copy(this.originalUser);
-    this.resetForm();
-};
-
-UserEditorController.prototype.remove = function(event) {
-    const $ctrl = this;
     
-    const confirm = this.$mdDialog.confirm()
-        .title(this.Translate.trSync('ui.app.areYouSure'))
-        .textContent(this.Translate.trSync('ui.components.confirmDeleteUser'))
-        .ariaLabel(this.Translate.trSync('ui.app.areYouSure'))
-        .targetEvent(event)
-        .ok(this.Translate.trSync('common.ok'))
-        .cancel(this.Translate.trSync('common.cancel'));
-
-    this.$mdDialog.show(confirm).then(function() {
-        const username = $ctrl.originalUser.username;
-        $ctrl.originalUser.$delete().then(function(user) {
-            $ctrl.user = null;
-            $ctrl.originalUser = null;
-            $ctrl.resetForm();
-            $ctrl.onDelete({$user: user});
-            
-            const toast = $ctrl.$mdToast.simple()
-                .textContent($ctrl.Translate.trSync('ui.components.userDeleted', username))
-                .action($ctrl.Translate.trSync('common.ok'))
-                .highlightAction(true)
-                .position('bottom center')
-                .hideDelay(10000);
-            $ctrl.$mdToast.show(toast);
-        }, function(response) {
-            const toast = $ctrl.$mdToast.simple()
-                .textContent($ctrl.Translate.trSync('ui.components.errorDeletingUser', username), response.mangoStatusText)
-                .action($ctrl.Translate.trSync('common.ok'))
-                .highlightAction(true)
-                .toastClass('md-warn')
-                .position('bottom center')
-                .hideDelay(10000);
-            $ctrl.$mdToast.show(toast);
+    revert() {
+        this.user = angular.copy(this.originalUser);
+        this.resetForm();
+    }
+    
+    remove(event) {
+        const confirm = this.$mdDialog.confirm()
+            .title(this.Translate.trSync('ui.app.areYouSure'))
+            .textContent(this.Translate.trSync('ui.components.confirmDeleteUser'))
+            .ariaLabel(this.Translate.trSync('ui.app.areYouSure'))
+            .targetEvent(event)
+            .ok(this.Translate.trSync('common.ok'))
+            .cancel(this.Translate.trSync('common.cancel'));
+    
+        this.$mdDialog.show(confirm).then(() => {
+            const username = this.originalUser.username;
+            this.originalUser.$delete().then(user => {
+                this.user = null;
+                this.originalUser = null;
+                this.resetForm();
+                this.onDelete({$user: user});
+    
+                this.maDialogHelper.toast(['ui.components.userDeleted', username]);
+            }, error => {
+                this.maDialogHelper.errorToast(['ui.components.errorDeletingUser', username, error.mangoStatusText]);
+            });
         });
-    });
-};
-
-UserEditorController.prototype.sendTestEmail = function() {
-    const $ctrl = this;
-    this.User.current.sendTestEmail().then(function(response) {
-        const toast = $ctrl.$mdToast.simple()
-            .textContent(response.data)
-            .action($ctrl.Translate.trSync('common.ok'))
-            .highlightAction(true)
-            .position('bottom center')
-            .hideDelay(10000);
-        $ctrl.$mdToast.show(toast);
-    }, function(response) {
-        const toast = $ctrl.$mdToast.simple()
-            .textContent($ctrl.Translate.trSync('ui.components.errorSendingEmail', this.User.current.email, response.mangoStatusText))
-            .action($ctrl.Translate.trSync('common.ok'))
-            .highlightAction(true)
-            .toastClass('md-warn')
-            .position('bottom center')
-            .hideDelay(10000);
-        $ctrl.$mdToast.show(toast);
-    }.bind(this));
-};
-
-UserEditorController.prototype.switchUser = function(event) {
-    const $ctrl = this;
-    const username = this.originalUser.username;
-    this.User.switchUser({username}).$promise.then(response => {
-        const toast = $ctrl.$mdToast.simple()
-            .textContent($ctrl.Translate.trSync('ui.components.switchedUser', username))
-            .action($ctrl.Translate.trSync('common.ok'))
-            .highlightAction(true)
-            .position('bottom center')
-            .hideDelay(10000);
-        $ctrl.$mdToast.show(toast);
-        
-        if (this.$state) {
-            // reload the resolves and views of this state and its parents
-            this.$state.go('.', null, {reload: true});
-        } else {
-            this.$window.location.reload();
-        }
-    }, response => {
-        const toast = $ctrl.$mdToast.simple()
-            .textContent($ctrl.Translate.trSync('ui.components.errorSwitchingUser', username, response.mangoStatusText))
-            .action($ctrl.Translate.trSync('common.ok'))
-            .highlightAction(true)
-            .toastClass('md-warn')
-            .position('bottom center')
-            .hideDelay(10000);
-        $ctrl.$mdToast.show(toast);
-    });
-};
-
-UserEditorController.prototype.regExpEscape = function(s) {
-    return String(s).replace(/[\\^$*+?.()|[\]{}]/g, '\\$&');
-};
+    }
+    
+    sendTestEmail() {
+        this.sendingEmail = true;
+        this.User.current.sendTestEmail().then(response => {
+            this.maDialogHelper.toastOptions({text: response.data});
+        }, error => {
+            this.maDialogHelper.errorToast(['ui.components.errorSendingEmail', this.User.current.email, error.mangoStatusText]);
+        }).finally(() => {
+            delete this.sendingEmail;
+        });
+    }
+    
+    switchUser(event) {
+        const username = this.originalUser.username;
+        this.User.switchUser({username}).$promise.then(response => {
+            this.maDialogHelper.toast(['ui.components.switchedUser', username]);
+    
+            if (this.$state) {
+                // reload the resolves and views of this state and its parents
+                this.$state.go('.', null, {reload: true});
+            } else {
+                this.$window.location.reload();
+            }
+        }, error => {
+            this.maDialogHelper.errorToast(['ui.components.errorSwitchingUser', username, error.mangoStatusText]);
+        });
+    }
+    
+    regExpEscape(s) {
+        return String(s).replace(/[\\^$*+?.()|[\]{}]/g, '\\$&');
+    }
+}
 
 export default {
     controller: UserEditorController,
