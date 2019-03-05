131eced069a3f94de2586dd8455c51d7a42e0f8e
Jared Wiltshire <jazdw@users.noreply.github.com>
2019-03-05 11:53:46 -0700

    Remove test defaults

--- UI/web-src/ngMango/components/treeView/treeView.js
+++ UI/web-src/ngMango/components/treeView/treeView.js
@@ -6,15 +6,6 @@
 import treeViewTemplate from './treeView.html';
 import './treeView.css';
 
-const defaultItems = [
-    {id: 1, children: [], label: 'item 1'},
-    {id: 2, children: [
-        {id: 21, children: [{id: 211, children: [], label: 'item 211'}], label: 'item 21'},
-        {id: 22, children: [], label: 'item 22'}
-    ], label: 'item 2'},
-    {id: 3, children: [], label: 'item 3'}
-];
-
 class TreeViewController {
     static get $$ngIsClass() { return true; }
     static get $inject() { return ['$scope', '$transclude']; }
@@ -25,7 +16,7 @@ class TreeViewController {
     }
     
     $onChanges(changes) {
-        this.$scope.children = this.items || defaultItems;
+        this.$scope.children = this.items;
     }
 
     id(item) {
