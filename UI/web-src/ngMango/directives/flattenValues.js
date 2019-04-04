6de72844f0872bae2730f0bc857b0ef17a6da530
Jared Wiltshire <jazdw@users.noreply.github.com>
2019-04-04 17:00:49 -0600

    Automatically add messages to md-checkbox and input[type=number]

--- UI/web-src/ngMango/directives/flattenValues.js
+++ UI/web-src/ngMango/directives/flattenValues.js
@@ -3,8 +3,15 @@
  * @author Jared Wiltshire
  */
 
-flattenValues.$inject = ['$parse', 'maMultipleValues'];
-function flattenValues($parse, MultipleValues) {
+flattenValues.$inject = ['$parse', 'maMultipleValues', '$compile'];
+function flattenValues($parse, MultipleValues, $compile) {
+    
+    const msgTemplate = `<div class="ma-small-text" ng-if="ngModel.$modelValue.hasMultipleValues()">
+                            <em ng-bind="ngModel.$modelValue.toString()"></em>
+                         </div>`;
+    
+    const linkFn = $compile(msgTemplate);
+    
     return {
         require: ['ngModel', '^?mdInputContainer'],
         restrict: 'A',
@@ -13,6 +20,20 @@ function flattenValues($parse, MultipleValues) {
             let expression;
             if ($attrs.maFlattenValues) {
                 expression = $parse($attrs.maFlattenValues);
+            }
+            
+            if ($element[0].tagName === 'INPUT' && $attrs.type === 'number' && $element.parent()[0].tagName === 'MD-INPUT-CONTAINER' ||
+                    $element[0].tagName === 'MD-CHECKBOX') {
+                
+                const scope = $scope.$new(true);
+                scope.ngModel = ngModel;
+                linkFn(scope, (cloned, scope) => {
+                    if ($element[0].tagName === 'MD-CHECKBOX') {
+                        $element.maFind('div.md-label').append(cloned);
+                    } else {
+                        $element.parent().append(cloned);
+                    }
+                });
             }
             
             ngModel.$formatters.push(function multipleValueFormatter(value) {
