354dc2b7caf6ab7c3282e4a4b8354619d84b817e
Luis Güette <35845741+guetteluis@users.noreply.github.com>
2019-04-11 13:14:37 -0400

    Serial port select component (#209)
    * add component and service files
    
    * finish component and service
    
    * serial port select finished

---
+++ UI/web-src/ngMango/components/serialPortsSelect/serialPortSelect.js
@@ -0,0 +1,62 @@
+/**
+ * @copyright 2018 {@link http://infiniteautomation.com|Infinite Automation Systems, Inc.} All rights reserved.
+ * @author Jared Wiltshire
+ */
+
+import serialPortSelectTemplate from './serialPortSelect.html';
+
+class SerialPortSelectController {
+    static get $$ngIsClass() { return true; }
+    static get $inject() { return ['maSerialPort', '$scope']; }
+    
+    constructor(maSerialPort, $scope) {
+        this.maSerialPort = maSerialPort;
+        this.$scope = $scope;
+    }
+    
+    $onInit() {
+        this.ngModelCtrl.$render = () => this.render();
+        
+        this.getSerialPorts();
+    }
+    
+    render() {
+        this.selected = this.ngModelCtrl.$viewValue;
+    }
+    
+    getSerialPorts() {
+        this.serialPortPromise = this.maSerialPort.list().then(serialPorts => {
+            this.serialPorts = serialPorts;
+            return serialPorts;
+        });
+    }
+
+    selectChanged() {
+        this.ngModelCtrl.$setViewValue(this.selected);
+    }
+    
+}
+
+export default {
+    controller: SerialPortSelectController,
+    template: serialPortSelectTemplate,
+    require: {
+        'ngModelCtrl': 'ngModel'
+    },
+    bindings: {
+        showIcon: '<?',
+        showClear: '<?',
+        selectMultiple: '<?',
+        hideName: '<?'
+    },
+    transclude: {
+        label: '?maLabel'
+    },
+    designerInfo: {
+        translation: 'ui.components.maSerialPortSelect',
+        icon: 'storage',
+        attributes: {
+            showClear: {type: 'boolean'}
+        }
+    }
+};
