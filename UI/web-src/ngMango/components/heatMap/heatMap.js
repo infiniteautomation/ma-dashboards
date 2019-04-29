5957a00969dff20cb8a2197f388e56276222177a
Jared Wiltshire <jazdw@users.noreply.github.com>
2019-04-29 15:32:29 -0600

    Control number of ticks and tick format

--- UI/web-src/ngMango/components/heatMap/heatMap.js
+++ UI/web-src/ngMango/components/heatMap/heatMap.js
@@ -8,6 +8,16 @@ import heatMapTemplate from './heatMap.html';
 import heatMapTooltip from './heatMapTooltip.html';
 import './heatMap.css';
 
+const arrayReduceToMap = function(fn) {
+    return this.reduce((map, v, i, array) => {
+        const value = fn(v, i, array);
+        if (value != null) {
+            map.set(v, value);
+        }
+        return map;
+    }, new Map());
+};
+
 class HeatMapController {
     static get $$ngIsClass() { return true; }
     static get $inject() { return ['$scope', '$element', '$transclude', '$compile']; }
@@ -176,13 +186,13 @@ class HeatMapController {
         this.xScale = d3.scaleBand()
             .range([0, graphWidth]);
         
-        this.xAxis = svg.select('g.ma-heat-map-x-axis')
+        svg.select('g.ma-heat-map-x-axis')
             .attr('transform', `translate(${margins.left}, ${margins.top})`);
 
         this.yScale = d3.scaleBand()
             .range([0, graphHeight]);
 
-        this.yAxis = svg.select('g.ma-heat-map-y-axis')
+        svg.select('g.ma-heat-map-y-axis')
             .attr('transform', `translate(${margins.left}, ${margins.top})`);
 
         this.updateColorScale();
@@ -240,18 +250,36 @@ class HeatMapController {
         }
     }
     
-    formatX(value) {
+    formatX(value, index) {
         if (typeof this.axisFormatX === 'function') {
-            return this.axisFormatX({$value: value});
+            return this.axisFormatX({$value: value, $index: index, $columns: this.columns});
         }
-        return value.format(this.groupBy.startsWith('day') ? 'l' : 'YYYY-w');
+        
+        if (this.groupBy.startsWith('day')) {
+            if (index % 7 === 0) {
+                return value.format('l');
+            }
+        } else {
+            return value.format('YYYY-w');
+        }
     }
     
-    formatY(value) {
+    formatY(value, index) {
         if (typeof this.axisFormatY === 'function') {
-            return this.axisFormatY({$value: value});
+            return this.axisFormatY({$value: value, $index: index, $rows: this.rows});
         }
-        return value.format(this.groupBy.startsWith('day') ? 'LT' : 'ddd LT');
+        
+        if (this.groupBy.startsWith('day')) {
+            // we want 8 tick marks i.e. every 3 hrs
+            if (index % Math.ceil(this.rows / 8) === 0) {
+                return value.format('LT');
+            }
+        } else {
+            // we want 7 tick marks i.e. every 1 day
+            if (index % Math.ceil(this.rows / 7) === 0) {
+                return value.format('ddd');
+            }
+        }
     }
     
     updateAxis() {
@@ -267,14 +295,25 @@ class HeatMapController {
             from = to = moment(0);
         }
 
-        const numColumns = Math.ceil(to.diff(from, this.groupBy, true));
-        const xDomain = Array(numColumns).fill().map((v, i) => {
-            const startTime = moment(from).add(i, this.groupBy).startOf(this.groupBy);
-            return this.formatX(startTime);
+        this.columns = Math.ceil(to.diff(from, this.groupBy, true));
+        const xDomain = Array(this.columns).fill().map((v, i) => {
+            return moment(from).add(i, this.groupBy).startOf(this.groupBy);
         });
 
-        this.xScale.domain(xDomain);
-        this.xAxis.call(d3.axisTop(this.xScale).tickSizeOuter(0));
+        this.xDomainFormat = this.groupBy.startsWith('day') ? 'YYYY-MM-DD' : 'YYYY-w';
+        this.xScale.domain(xDomain.map(m => m.format(this.xDomainFormat)));
+        
+        const xScaleMomentDomain = this.xScale.copy()
+            .domain(xDomain);
+
+        const xDomainFormatted = arrayReduceToMap.call(xDomain, (v, i) => this.formatX(v, i));
+        const xAxis = d3.axisTop(xScaleMomentDomain)
+            .tickValues(xDomain.filter(v => xDomainFormatted.has(v)))
+            .tickFormat(v => xDomainFormatted.get(v))
+            .tickSizeOuter(0);
+        
+        this.svg.select('g.ma-heat-map-x-axis')
+            .call(xAxis);
         
         // setup the Y axis
         
@@ -282,30 +321,42 @@ class HeatMapController {
         const yEnd = moment(yStart).add(1, this.groupBy);
         const yDuration = yEnd - yStart;
 
-        let numRows = 48;
-        if (this.rows) {
-            numRows = this.rows;
+        this.rows = 48;
+        if (this.rowsAttr) {
+            this.rows = this.rowsAttr;
         } else if (Array.isArray(this.pointValues) && this.pointValues.$options && this.pointValues.$options.rollupInterval) {
             // automatically set the number of rows based on the rollup interval used
             const rollupInterval = this.pointValues.$options.rollupInterval;
             try {
                 const [interval, units] = rollupInterval.trim().split(/\s+/);
                 const rollupDuration = moment.duration(Number.parseInt(interval, 10), units);
-                numRows = Math.floor(yDuration / rollupDuration);
+                this.rows = Math.floor(yDuration / rollupDuration);
             } catch (e) {}
         } else if (Array.isArray(this.pointValues) && this.pointValues.length > 1) {
             // automatically set the number of rows based on the time difference between two point values
             const diff = this.pointValues[1].timestamp - this.pointValues[0].timestamp;
-            numRows = Math.floor(yDuration / diff);
+            this.rows = Math.floor(yDuration / diff);
         }
         
-        const yDomain = Array(numRows).fill().map((v, i) => {
-            const ms = yDuration / numRows * i;
-            return this.formatY(moment(yStart + ms).utc().utcOffset(0));
+        const yDomain = Array(this.rows).fill().map((v, i) => {
+            const ms = yDuration / this.rows * i;
+            return moment(yStart + ms).utc().utcOffset(0);
         });
         
-        this.yScale.domain(yDomain);
-        this.yAxis.call(d3.axisLeft(this.yScale).tickSizeOuter(0));
+        this.yDomainFormat = this.groupBy.startsWith('day') ? 'HH:mm' : 'dd HH:mm';
+        this.yScale.domain(yDomain.map(m => m.format(this.yDomainFormat)));
+        
+        const yScaleMomentDomain = this.yScale.copy()
+            .domain(yDomain);
+
+        const yDomainFormatted = arrayReduceToMap.call(yDomain, (v, i) => this.formatY(v, i));
+        const yAxis = d3.axisLeft(yScaleMomentDomain)
+            .tickValues(yDomain.filter(v => yDomainFormatted.has(v)))
+            .tickFormat(v => yDomainFormatted.get(v))
+            .tickSizeOuter(0);
+
+        this.svg.select('g.ma-heat-map-y-axis')
+            .call(yAxis);
     }
     
     updateGraph() {
@@ -357,12 +408,11 @@ class HeatMapController {
         const xBandWidth = this.xScale.bandwidth() * 1.03;
         const yBandWidth = this.yScale.bandwidth() * 1.03;
 
-        // can split this up into updates just for value or just for scale updates
         rects.merge(newRects)
             .attr('transform', pv => {
                 const time = this.setTimezone(moment(pv.timestamp));
-                const x = this.xScale(this.formatX(time));
-                const y = this.yScale(this.formatY(time));
+                const x = this.xScale(time.format(this.xDomainFormat));
+                const y = this.yScale(time.format(this.yDomainFormat));
                 return `translate(${x}, ${y})`;
             })
             .attr('width', xBandWidth)
@@ -436,7 +486,7 @@ export default {
         timezone: '@?',
         utcOffset: '<?',
         groupBy: '@?',
-        rows: '<?',
+        rowsAttr: '<?rows',
         autoScale: '<?',
         pointValues: '<',
         minValueAttr: '<?minValue',
