c5ca719c9a5573e7c9f5dbd974285cf70c024b20
Jared Wiltshire <jazdw@users.noreply.github.com>
2019-05-17 15:29:12 -0600

    Use v2 API for statistics
    Closes #244

--- UI/web-src/ngMango/services/statistics.js
+++ UI/web-src/ngMango/services/statistics.js
@@ -1,66 +1,78 @@
 /**
- * @copyright 2018 {@link http://infiniteautomation.com|Infinite Automation Systems, Inc.} All rights reserved.
+ * @copyright 2019 {@link http://infiniteautomation.com|Infinite Automation Systems, Inc.} All rights reserved.
  * @author Jared Wiltshire
  */
 
 statisticsFactory.$inject = ['$http', '$q', 'maUtil'];
 function statisticsFactory($http, $q, Util) {
-    const pointValuesUrl = '/rest/v1/point-values/';
+    const pointValuesUrl = '/rest/v2/point-values/';
 
-    function Statistics() {
-    }
+    class Statistics {
+        static getStatisticsForXid(xid, options) {
+            try {
+                if (typeof xid !== 'string') throw new Error('Requires xid parameter');
+                if (!options || typeof options !== 'object') throw new Error('Requires options parameter');
+                
+                const url = `${pointValuesUrl}/statistics/${encodeURIComponent(xid)}`;
+                const params = {
+                    fields: ['VALUE', 'RENDERED', 'TIMESTAMP', 'ANNOTATION']
+                };
+                
+                if (options.from !== undefined && options.to !== undefined) {
+                    const now = new Date();
+                    const from = Util.toMoment(options.from, now, options.dateFormat);
+                    const to = Util.toMoment(options.to, now, options.dateFormat);
+                    
+                    if (from.valueOf() === to.valueOf()) {
+                        return $q.when({});
+                    }
+                    
+                    params.from = from.toISOString();
+                    params.to = to.toISOString();
+                } else {
+                    throw new Error('Requires options.to and options.from');
+                }
+
+                const canceler = $q.defer();
+                const cancelOrTimeout = Util.cancelOrTimeout(canceler.promise, options.timeout);
     
-    Statistics.prototype.getStatisticsForXid = function getStatisticsForXid(xid, options) {
-        try {
-            if (typeof xid !== 'string') throw new Error('Requires xid parameter');
-            if (!options || typeof options !== 'object') throw new Error('Requires options parameter');
-            
-            let url = pointValuesUrl + encodeURIComponent(xid) + (options.firstLast ? '/first-last' : '/statistics');
-            const params = [];
-            
-            if (options.from !== undefined && options.to !== undefined) {
-                const now = new Date();
-                const from = Util.toMoment(options.from, now, options.dateFormat);
-                const to = Util.toMoment(options.to, now, options.dateFormat);
-                
-                if (from.valueOf() === to.valueOf()) {
-                    return $q.when({});
-                }
-                
-                params.push('from=' + encodeURIComponent(from.toISOString()));
-                params.push('to=' + encodeURIComponent(to.toISOString()));
-            } else {
-                throw new Error('Requires options.to and options.from');
-            }
-            
-            if (options.rendered || options.rendered === undefined) {
-                params.push('useRendered=true');
-            }
-            
-            url += '?' + params.join('&');
-            
-            const canceler = $q.defer();
-            const cancelOrTimeout = Util.cancelOrTimeout(canceler.promise, options.timeout);
+                return $http({
+                    url,
+                    timeout: cancelOrTimeout,
+                    params
+                }).then(response => {
+                    if (!response || !response.data) {
+                        throw new Error('Incorrect response from REST end point ' + url);
+                    }
 
-            return $http.get(url, {
-                timeout: cancelOrTimeout,
-                headers: {
-                    'Accept': 'application/json'
-                },
-                cache: true
-            }).then(function(response) {
-                if (!response || !response.data) {
-                    throw new Error('Incorrect response from REST end point ' + url);
-                }
-                return response.data;
-            }).setCancel(canceler.resolve);
-        } catch (error) {
-            return $q.reject(error);
+                    const stats = response.data[xid];
+                    stats.hasData = !!stats.count; // for legacy purposes
+                    
+                    // convert to legacy output format by default, move rendered property to value property
+                    if (options.rendered || options.rendered === undefined) {
+                        Object.keys(stats).forEach(key => {
+                            const stat = stats[key];
+                            if (stat != null && typeof stat === 'object' && typeof stat.rendered === 'string') {
+                                stat.value = stat.rendered;
+                                delete stat.rendered;
+                            }
+                        });
+                    }
+                    
+                    if (options.firstLast) {
+                        return [stats.first, stats.last];
+                    }
+                    
+                    return stats;
+                }).setCancel(canceler.resolve);
+                
+            } catch (error) {
+                return $q.reject(error);
+            }
         }
-    };
+    }
 
-    return new Statistics();
+    return Statistics;
 }
 
 export default statisticsFactory;
-
