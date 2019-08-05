297ec30f6e361e2ae660362a44b53552ec2beeb8
Jared Wiltshire <jazdw@users.noreply.github.com>
2019-08-05 13:58:39 -0600

    Flex by default

--- UI/web-src/ngMango/services/Publisher.js
+++ UI/web-src/ngMango/services/Publisher.js
@@ -166,7 +166,7 @@ function publisherProvider() {
                 if (Array.isArray(this.pointProperties)) {
                     this.pointProperties.forEach(pp => {
                         if (!pp.editorTemplate) {
-                            pp.editorTemplate = `<md-input-container><input ng-model="publisherPoint[pointProperty.name]"></md-input-container>`;
+                            pp.editorTemplate = `<md-input-container flex><input ng-model="publisherPoint[pointProperty.name]"></md-input-container>`;
                         }
                         pp.editorTemplateUrl = `publisherEditor.${this.type}.${pp.name}.html`;
                         $templateCache.put(pp.editorTemplateUrl, pp.editorTemplate);
