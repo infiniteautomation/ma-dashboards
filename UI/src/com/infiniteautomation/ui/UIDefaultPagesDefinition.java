cb5da5329f372bd7f227ab808772023ec0f6e30b
Jared Wiltshire <jazdw@users.noreply.github.com>
2019-08-15 10:48:10 -0600

    Redirect un-authenticated users to /ui instead of /ui/login

--- UI/src/com/infiniteautomation/ui/UIDefaultPagesDefinition.java
+++ UI/src/com/infiniteautomation/ui/UIDefaultPagesDefinition.java
@@ -18,131 +18,105 @@ import com.serotonin.m2m2.vo.User;
  * @author Terry Packer
  */
 public class UIDefaultPagesDefinition extends DefaultPagesDefinition {
-    
-	@Override
+
+    @Override
     public String getLoginPageUri(HttpServletRequest request, HttpServletResponse response) {
-    	
-		String loginPage = SystemSettingsDao.instance.getValue(UICommon.UI_LOGIN_PAGE);
-		
-		if(!StringUtils.isEmpty(loginPage)){
-			return loginPage;
-		}else{
-			return null;
-		}
+
+        String loginPage = SystemSettingsDao.instance.getValue(UICommon.UI_LOGIN_PAGE);
+
+        if(!StringUtils.isEmpty(loginPage)){
+            return loginPage;
+        }else{
+            return null;
+        }
     }
-	
-	@Override
-	public String getPasswordResetPageUri() {
-	    String passwordResetPage = SystemSettingsDao.instance.getValue(UICommon.UI_PASSWORD_RESET_PAGE);
-        
+
+    @Override
+    public String getPasswordResetPageUri() {
+        String passwordResetPage = SystemSettingsDao.instance.getValue(UICommon.UI_PASSWORD_RESET_PAGE);
+
         if(!StringUtils.isEmpty(passwordResetPage)){
             return passwordResetPage;
         }else{
             return null;
         }
     }
-	
-	/* (non-Javadoc)
-	 * @see com.serotonin.m2m2.module.DefaultPagesDefinition#getFirstLoginPageUri(javax.servlet.http.HttpServletRequest, javax.servlet.http.HttpServletResponse)
-	 */
-	@Override
-	public String getFirstLoginPageUri(HttpServletRequest request, HttpServletResponse response) {
-	    return "/ui/agree-to-license";
-	}
-	
-	/* (non-Javadoc)
-	 * @see com.serotonin.m2m2.module.DefaultPagesDefinition#getLoggedInPageUri(javax.servlet.http.HttpServletRequest, javax.servlet.http.HttpServletResponse, com.serotonin.m2m2.vo.User)
-	 */
-	@Override
-	public String getLoggedInPageUri(HttpServletRequest request, HttpServletResponse response, User user) {
-		String page = SystemSettingsDao.instance.getValue(UICommon.UI_LOGGED_IN_PAGE);
-		
-		if(!StringUtils.isEmpty(page)){
-			return page;
-		}else{
-			return null;
-		}
-	}
 
-	/* (non-Javadoc)
-	 * @see com.serotonin.m2m2.module.DefaultPagesDefinition#getFirstUserLoginPageUri(javax.servlet.http.HttpServletRequest, javax.servlet.http.HttpServletResponse, com.serotonin.m2m2.vo.User)
-	 */
-	@Override
-	public String getFirstUserLoginPageUri(HttpServletRequest request, HttpServletResponse response, User user) {
-		String page = SystemSettingsDao.instance.getValue(UICommon.UI_FIRST_USER_LOGIN_PAGE);
-		
-		if(!StringUtils.isEmpty(page)){
-			return page;
-		}else{
-			return null;
-		}
-	}
+    @Override
+    public String getFirstLoginPageUri(HttpServletRequest request, HttpServletResponse response) {
+        return "/ui/agree-to-license";
+    }
 
-	/* (non-Javadoc)
-	 * @see com.serotonin.m2m2.module.DefaultPagesDefinition#getLoggedInPageUriPreHome(javax.servlet.http.HttpServletRequest, javax.servlet.http.HttpServletResponse, com.serotonin.m2m2.vo.User)
-	 */
-	@Override
-	public String getLoggedInPageUriPreHome(HttpServletRequest request, HttpServletResponse response, User user) {
-		String page = SystemSettingsDao.instance.getValue(UICommon.UI_LOGGED_IN_PAGE_PRE_HOME);
-		
-		if(!StringUtils.isEmpty(page)){
-			return page;
-		}else{
-			return null;
-		}
-	}
-	
-	/* (non-Javadoc)
-	 * @see com.serotonin.m2m2.module.DefaultPagesDefinition#getUnauthorizedPageUri(javax.servlet.http.HttpServletRequest, javax.servlet.http.HttpServletResponse, com.serotonin.m2m2.vo.User)
-	 */
-	@Override
-	public String getUnauthorizedPageUri(HttpServletRequest request, HttpServletResponse response, User user) {
-		String page = SystemSettingsDao.instance.getValue(UICommon.UI_UNAUTHORIZED_PAGE);
-		
-		if(!StringUtils.isEmpty(page)){
-			return page;
-		}else{
-			return null;
-		}
-	}	
-	
-	/* (non-Javadoc)
-	 * @see com.serotonin.m2m2.module.DefaultPagesDefinition#getNotFoundPageUri(javax.servlet.http.HttpServletRequest, javax.servlet.http.HttpServletResponse)
-	 */
-	@Override
-	public String getNotFoundPageUri(HttpServletRequest request, HttpServletResponse response) {
-		String page = SystemSettingsDao.instance.getValue(UICommon.UI_NOT_FOUND_PAGE);
-		
-		if(!StringUtils.isEmpty(page)){
-			String requested = request.getRequestURI();
-			return UriComponentsBuilder.fromPath(page).queryParam("path", requested).toUriString();
-		}else{
-			return null;
-		}
+    @Override
+    public String getLoggedInPageUri(HttpServletRequest request, HttpServletResponse response, User user) {
+        String page = SystemSettingsDao.instance.getValue(UICommon.UI_LOGGED_IN_PAGE);
 
-	}
-	
-	/* (non-Javadoc)
-	 * @see com.serotonin.m2m2.module.DefaultPagesDefinition#getErrorPageUri(javax.servlet.http.HttpServletRequest, javax.servlet.http.HttpServletResponse)
-	 */
-	@Override
-	public String getErrorPageUri(HttpServletRequest request, HttpServletResponse response) {
-		String page = SystemSettingsDao.instance.getValue(UICommon.UI_ERROR_PAGE);
-		
-		if(!StringUtils.isEmpty(page)){
-			return page;
-		}else{
-			return null;
-		}
+        if(!StringUtils.isEmpty(page)){
+            return page;
+        }else{
+            return null;
+        }
+    }
 
-	}
-	
-	/* (non-Javadoc)
-	 * @see com.serotonin.m2m2.module.DefaultPagesDefinition#getStartupPageUri(javax.servlet.http.HttpServletRequest, javax.servlet.http.HttpServletResponse)
-	 */
-	@Override
-	public String getStartupPageUri(HttpServletRequest request, HttpServletResponse response) {
-		return null; //TODO Configure this as a hard URL when ready.  This cannot get info from the DB since the DB will not be ready yet
-	}
-	
+    @Override
+    public String getFirstUserLoginPageUri(HttpServletRequest request, HttpServletResponse response, User user) {
+        String page = SystemSettingsDao.instance.getValue(UICommon.UI_FIRST_USER_LOGIN_PAGE);
+
+        if(!StringUtils.isEmpty(page)){
+            return page;
+        }else{
+            return null;
+        }
+    }
+
+    @Override
+    public String getLoggedInPageUriPreHome(HttpServletRequest request, HttpServletResponse response, User user) {
+        String page = SystemSettingsDao.instance.getValue(UICommon.UI_LOGGED_IN_PAGE_PRE_HOME);
+
+        if(!StringUtils.isEmpty(page)){
+            return page;
+        }else{
+            return null;
+        }
+    }
+
+    @Override
+    public String getUnauthorizedPageUri(HttpServletRequest request, HttpServletResponse response, User user) {
+        String page = SystemSettingsDao.instance.getValue(UICommon.UI_UNAUTHORIZED_PAGE);
+
+        if(!StringUtils.isEmpty(page)){
+            return page;
+        }else{
+            return null;
+        }
+    }
+
+    @Override
+    public String getNotFoundPageUri(HttpServletRequest request, HttpServletResponse response) {
+        String page = SystemSettingsDao.instance.getValue(UICommon.UI_NOT_FOUND_PAGE);
+
+        if(!StringUtils.isEmpty(page)){
+            String requested = request.getRequestURI();
+            return UriComponentsBuilder.fromPath(page).queryParam("path", requested).toUriString();
+        }else{
+            return null;
+        }
+    }
+
+    @Override
+    public String getErrorPageUri(HttpServletRequest request, HttpServletResponse response) {
+        String page = SystemSettingsDao.instance.getValue(UICommon.UI_ERROR_PAGE);
+
+        if(!StringUtils.isEmpty(page)){
+            return page;
+        }else{
+            return null;
+        }
+
+    }
+
+    @Override
+    public String getStartupPageUri(HttpServletRequest request, HttpServletResponse response) {
+        return null; //TODO Configure this as a hard URL when ready.  This cannot get info from the DB since the DB will not be ready yet
+    }
 }
