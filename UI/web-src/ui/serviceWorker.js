442846480cae10e203b2e30ae854c7a034d161fb
Jared Wiltshire <jazdw@users.noreply.github.com>
2019-08-28 16:35:34 -0600

    Remove old module cache objects when installing

--- UI/web-src/ui/serviceWorker.js
+++ UI/web-src/ui/serviceWorker.js
@@ -9,7 +9,7 @@ workbox.core.skipWaiting();
 workbox.core.clientsClaim();
 
 // used to delete outdated versions from a cache whenever a new version is written
-// still need to add a limit to expire files which are no longer used
+// Needed as the cleanUpModules() function only runs when the UI module is updated and service worker is installed
 class DeleteOutdatedVersions {
     constructor(cacheName) {
         this.cacheName = cacheName;
@@ -19,21 +19,17 @@ class DeleteOutdatedVersions {
         if (mode === 'write') {
             return caches.open(this.cacheName).then(cache => {
                 // delete all requests with matching URL from cache, ignoring the ?v= search parameter
-                return cache.delete(request, {ignoreVary: true, ignoreSearch: true});
+                return cache.delete(request, {ignoreSearch: true});
             }).then(() => request);
         }
-        return request;
+        return Promise.resolve(request);
     }
 }
 
 const moduleResourcesCacheName = 'module-resources';
 
-// Mango sends a Vary: User-Agent header, need to ignore it for caching our resources
 const moduleResourcesStrategy = new workbox.strategies.CacheFirst({
     cacheName: moduleResourcesCacheName,
-    matchOptions: {
-        ignoreVary: true
-    },
     plugins: [
         new DeleteOutdatedVersions(moduleResourcesCacheName)
     ]
@@ -53,15 +49,49 @@ workbox.routing.registerNavigationRoute(
     workbox.precaching.getCacheKeyForURL('/ui/index.html')
 );
 
-self.addEventListener('install', event => {
-    // warm up the module-resources cache by requesting and caching the files defined in AngularJSModuleDefinitions
-    const done = fetch('/rest/v1/modules/angularjs-modules/public').then(r => r.json()).then(modules => {
-        const requests = modules.urls.map(url => {
-            return moduleResourcesStrategy.makeRequest({
-                request: url
+const moduleForUrl = (url) => {
+    const matches = /\/modules\/([\w-]+)\/web\//.exec(url);
+    return matches && matches[1];
+};
+
+const cleanUpModules = () => {
+    fetch('/rest/v1/modules/angularjs-modules/public').then(r => r.json()).then(modules => {
+        return caches.open(moduleResourcesCacheName).then(cache => {
+            const updatedModulesPromise = Promise.all(modules.urls.map(url => {
+                return cache.match(url).then(response => {
+                    if (!response) {
+                        return moduleForUrl(url);
+                    }
+                });
+            }));
+            return Promise.all([cache, cache.keys(), updatedModulesPromise]);
+        }).then(([cache, keys, updated]) => {
+            const moduleNames = new Set(modules.modules.map(m => m.name));
+            const updatedModules = new Set(updated.filter(m => !!m));
+
+            const deletePromises = keys.map(k => {
+                const moduleName = moduleForUrl(k.url);
+                if (moduleName) {
+                    // remove all entries for modules which have been updated or deleted
+                    if (!moduleNames.has(moduleName) || updatedModules.has(moduleName)) {
+                        return cache.delete(k);
+                    }
+                }
+            }).filter(p => !!p);
+
+            return Promise.all(deletePromises);
+        }).then(() => {
+            // warm up the module-resources cache by requesting and caching the files defined in AngularJSModuleDefinitions
+            const requests = modules.urls.map(url => {
+                return moduleResourcesStrategy.makeRequest({
+                    request: url
+                });
             });
+            return Promise.all(requests);
         });
-        return Promise.all(requests);
     });
-    event.waitUntil(done);
+};
+
+self.addEventListener('install', event => {
+    event.waitUntil(cleanUpModules());
 });
