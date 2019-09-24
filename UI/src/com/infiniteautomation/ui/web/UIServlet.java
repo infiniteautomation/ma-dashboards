7ff250c2b4e049d83e70b947c5251ef16a4357cc
Jared Wiltshire <jazdw@users.noreply.github.com>
2019-09-24 10:51:44 -0600

    Update servlet for 3.7

--- UI/src/com/infiniteautomation/ui/web/UIServlet.java
+++ UI/src/com/infiniteautomation/ui/web/UIServlet.java
@@ -5,7 +5,6 @@ package com.infiniteautomation.ui.web;
 
 import java.io.IOException;
 import java.net.URL;
-import java.util.Objects;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
@@ -13,10 +12,8 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.eclipse.jetty.servlet.DefaultServlet;
 import org.springframework.http.HttpHeaders;
-import org.springframework.web.filter.ShallowEtagHeaderFilter;
 
-import com.serotonin.m2m2.Common;
-import com.serotonin.m2m2.web.filter.MangoShallowEtagHeaderFilter;
+import com.serotonin.m2m2.web.filter.MangoCacheControlHeaderFilter;
 import com.serotonin.m2m2.web.mvc.spring.security.BrowserRequestMatcher;
 
 /**
@@ -25,22 +22,12 @@ import com.serotonin.m2m2.web.mvc.spring.security.BrowserRequestMatcher;
 public class UIServlet extends DefaultServlet {
     private static final long serialVersionUID = 4078660846403316188L;
 
-    private final String forwardFrom;
-    private final String forwardTo;
-    private final String defaultPath;
-    private final String defaultHeaderValue = String.format(MangoShallowEtagHeaderFilter.MAX_AGE_TEMPLATE, Common.envProps.getLong("web.cache.maxAge", 0L));
-    // use the versioned resource setting as we are assuming everything loaded from /ui is versioned
-    private final String resourceHeaderValue = String.format(MangoShallowEtagHeaderFilter.MAX_AGE_TEMPLATE, Common.envProps.getLong("web.cache.maxAge.versionedResources", 31536000L));
-
-    public UIServlet(String forwardFrom, String forwardTo, String defaultPath) {
-        this.forwardFrom = Objects.requireNonNull(forwardFrom);
-        this.forwardTo = Objects.requireNonNull(forwardTo);
-        this.defaultPath = Objects.requireNonNull(defaultPath);
-    }
+    private static final String FORDWARD_TO_PATH = "/modules/mangoUI/web";
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         String requestURI = req.getRequestURI();
+        String forwardFrom = req.getServletPath();
 
         if (!requestURI.startsWith(forwardFrom)) {
             getServletContext().getRequestDispatcher(requestURI).forward(req, resp);
@@ -49,33 +36,26 @@ public class UIServlet extends DefaultServlet {
 
         String relativePath = requestURI.substring(forwardFrom.length());
 
-        // Jetty handles forwarding / to /index.html
-        URL resourceUrl = getServletContext().getResource(forwardTo + relativePath);
 
-        // if the resource is not a real file inside forwardFrom
+        URL resourceUrl = getServletContext().getResource(FORDWARD_TO_PATH + relativePath);
+
+        // if the resource is not an actual file we return the index.html file so the webapp can display a 404 not found message
         if (resourceUrl == null) {
+            // only redirect to the index.html file if its a browser request
             if (BrowserRequestMatcher.INSTANCE.matches(req)) {
-                relativePath = defaultPath;
+                relativePath = "/";
             } else {
                 resp.setStatus(HttpServletResponse.SC_NOT_FOUND);
                 return;
             }
         }
 
-        String cacheControlHeader;
-        if ("/serviceWorker.js".equals(relativePath) || defaultPath.equals(relativePath) || "/".equals(relativePath)) {
+        if ("/serviceWorker.js".equals(relativePath) || "/".equals(relativePath)) {
             // always re-validate cache for serviceWorker.js and index.html
-            cacheControlHeader = defaultHeaderValue;
-        } else {
-            // treat any other file as a versioned resource
-            cacheControlHeader = resourceHeaderValue;
+            // Override the Cache-Control header that was set by the EtagHeaderFilter
+            resp.setHeader(HttpHeaders.CACHE_CONTROL, String.format(MangoCacheControlHeaderFilter.MAX_AGE_TEMPLATE, 0));
         }
 
-        // Disable the Etag header and rely on last modified time only
-        ShallowEtagHeaderFilter.disableContentCaching(req);
-        // Override the Cache-Control header that was set by the EtagHeaderFilter
-        resp.setHeader(HttpHeaders.CACHE_CONTROL, cacheControlHeader);
-
-        getServletContext().getRequestDispatcher(forwardTo + relativePath).forward(req, resp);
+        getServletContext().getRequestDispatcher(FORDWARD_TO_PATH + relativePath).forward(req, resp);
     }
 }
--- UI/src/com/infiniteautomation/ui/web/UIServletDefinition.java
+++ UI/src/com/infiniteautomation/ui/web/UIServletDefinition.java
@@ -14,11 +14,10 @@ import com.serotonin.m2m2.module.ServletDefinition;
 public class UIServletDefinition extends ServletDefinition {
 
     private static final String FORWARD_FROM_PATH = "/ui";
-    private static final String FORDWARD_TO_PATH = "/modules/mangoUI/web";
 
     @Override
     public HttpServlet getServlet() {
-        return new UIServlet(FORWARD_FROM_PATH, FORDWARD_TO_PATH, "/index.html");
+        return new UIServlet();
     }
 
     @Override
