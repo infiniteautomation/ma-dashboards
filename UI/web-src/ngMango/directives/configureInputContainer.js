58173804de5505a349020bbf6ce8c0f0ef6e0b21
Jared Wiltshire <jazdw@users.noreply.github.com>
2020-06-22 16:54:02 -0600

    Change default hasValue function

--- UI/web-src/ngMango/directives/configureInputContainer.js
+++ UI/web-src/ngMango/directives/configureInputContainer.js
@@ -68,7 +68,7 @@ function configureInputContainerDirective(maUtil) {
                     if (this.$attrs.hasOwnProperty('hasValue')) {
                         hasValue = this.$scope.$eval(this.$attrs.hasValue, {$value: value});
                     } else {
-                        hasValue = this.$element[0].hasAttribute('multiple') ? Array.isArray(value) && value.length : value !== undefined;
+                        hasValue = this.$element[0].hasAttribute('multiple') ? Array.isArray(value) && value.length : value != null;
                     }
                     containerCtrl.setHasValue(!!hasValue);
                     return value;
