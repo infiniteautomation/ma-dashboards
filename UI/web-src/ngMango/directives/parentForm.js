4110eaedd300396c4d52d95aefaf68c298512bf1
Jared Wiltshire <jazdw@users.noreply.github.com>
2020-07-14 11:57:59 -0600

    Run link fn after ngModel, allow use on form

--- UI/web-src/ngMango/directives/parentForm.js
+++ UI/web-src/ngMango/directives/parentForm.js
@@ -5,36 +5,31 @@
 
 parentFormDirective.$inject = [];
 function parentFormDirective() {
-
-    class ParentFormController {
-        $onChanges(changes) {
-            if (changes.parentForm) {
-                if (changes.parentForm.isFirstChange()) {
-                    // for the first change we are running before the ngModelPreLink function
-                    if (this.parentForm) {
-                        // just set the $$parentForm property and let ngModelPreLink call $addControl()
-                        this.ngModel.$$parentForm = this.parentForm;
-                    }
-                } else {
-                    // ensure the control is removed from the previous form
-                    this.ngModel.$$parentForm.$removeControl(this.ngModel);
-                    if (this.parentForm) {
-                        this.parentForm.$addControl(this.ngModel);
-                    }
-                }
-            }
-        }
-    }
-    
     return {
         restrict: 'A',
         scope: false,
-        controller: ParentFormController,
-        bindToController: {
-            parentForm: '<maParentForm'
-        },
         require: {
-            ngModel: 'ngModel'
+            ngModel: '?ngModel',
+            form: '?form'
+        },
+        link: function($scope, $element, $attrs, controllers) {
+            const ngModel = controllers.ngModel;
+            const form = controllers.form;
+
+            $scope.$watch($attrs.maParentForm, (parentForm) => {
+                if (ngModel) {
+                    ngModel.$$parentForm.$removeControl(ngModel);
+                    if (parentForm) {
+                        parentForm.$addControl(ngModel);
+                    }
+                }
+                if (form) {
+                    form.$$parentForm.$removeControl(form);
+                    if (parentForm) {
+                        parentForm.$addControl(form);
+                    }
+                }
+            });
         }
     };
 }
