cf795f47c54e409dc2e8adf40c29cec4af287f60
Jared Wiltshire <jazdw@users.noreply.github.com>
2020-08-13 17:37:21 -0600

    WebSocket updates to role hierarchy

--- UI/web-src/ngMango/components/roleSelector/roleSelector.js
+++ UI/web-src/ngMango/components/roleSelector/roleSelector.js
@@ -16,10 +16,58 @@ class RoleSelectorController {
         this.maRole = maRole;
 
         this.selected = new Map();
+
+        // maps Role to inherited Role[]
+        this.inherited = new WeakMap();
+        this.limit = 100;
     }
     
     $onInit() {
         this.ngModelCtrl.$render = () => this.render();
+
+        this.maRole.notificationManager.subscribe((event, item, attributes) => {
+            switch(event.name) {
+                case 'create': {
+                    // add to root
+                    const matches = !this.filter || item.name.includes(this.filter.toLowerCase());
+                    if (matches) {
+                        this.addToRoot(item);
+                    }
+                    if (!this.filter) {
+                        // need to remove the inherited roles from root (if they exist there)
+                        item.inherited.forEach(xid => this.removeFromRoot(xid));
+                    }
+                    break;
+                }
+                case 'update': {
+                    const existing = this.findRole(attributes.originalXid);
+                    if (existing) {
+                        Object.assign(existing, item);
+                        existing.setOriginalId(item.xid);
+
+                        this.updateInherited(existing);
+
+                        if (!this.filter) {
+                            // any inherited roles should not be in root
+                            existing.inherited.forEach(xid => this.removeFromRoot(xid));
+                        }
+                    }
+                    break;
+                }
+                case 'delete': {
+                    // any roles which inherited the deleted role should get an update event, dont deal with that
+
+                    // remove the role from the root if it exists there
+                    this.removeFromRoot(item.xid);
+
+                    if (!this.filter && item.inherited.length) {
+                        // roles may no longer be inherited by any other role, they may be in the root now
+                        this.reloadChildren(this.root);
+                    }
+                    break;
+                }
+            }
+        }, this.$scope);
     }
 
     $onChanges(changes) {
@@ -63,9 +111,113 @@ class RoleSelectorController {
         }
     }
 
-    loadRoles(isRoot, role, existing) {
-        const builder = this.maRole.buildQuery();
+    findRole(xid, parent = this.root) {
+        const children = this.inherited.get(parent);
+        if (children) {
+            for (const role of children) {
+                if (role.xid === xid) {
+                    return role;
+                }
 
+                // depth first search
+                const found = this.findRole(xid, role);
+                if (found) {
+                    return found;
+                }
+            }
+        }
+    }
+
+    removeFromRoot(xid) {
+        const children = this.inherited.get(this.root);
+        if (children) {
+            const index = children.findIndex(r => r.xid === xid);
+            if (index >= 0) {
+                children.splice(index, 1);
+                children.$total--;
+            }
+        }
+    }
+
+    addToRoot(role) {
+        const children = this.inherited.get(this.root);
+        if (children) {
+            children.push(role);
+            children.$total++;
+            this.sortByName(children);
+        }
+    }
+
+    updateInherited(role) {
+        if (this.inherited.has(role)) {
+            const children = this.inherited.get(role);
+
+            let reloadRole = false;
+            let reloadRoot = false;
+
+            for (const xid of role.inherited) {
+                if (!children.some(c => c.xid === xid)) {
+                    // role was added to inherited
+                    const cached = this.findRole(xid);
+                    if (cached) {
+                        children.push(cached);
+                        children.$total++;
+                    } else {
+                        // a role was added that is not already in our cache, we will have to reload this role
+                        reloadRole = true;
+                    }
+                }
+            }
+
+            for (const child of children) {
+                if (!role.inherited.some(xid => xid === child.xid)) {
+                    // role was removed from inherited
+                    const i = children.indexOf(child);
+                    children.splice(i, 1);
+                    children.$total--;
+
+                    // role may no longer be inherited by any other role, it may be in the root now
+                    if (!this.filter) {
+                        reloadRoot = true;
+                    }
+                }
+            }
+
+            this.sortByName(children);
+
+            if (reloadRole) {
+                this.reloadChildren(role);
+            }
+            if (reloadRoot) {
+                this.reloadChildren(this.root);
+            }
+        }
+    }
+
+    sortByName(roles) {
+        roles.sort((a, b) => {
+            const nameA = a.name.toLowerCase();
+            const nameB = b.name.toLowerCase();
+            if (nameA < nameB) return -1;
+            if (nameA > nameB) return 1;
+            return 0;
+        });
+        return roles;
+    }
+
+    loadRoles(isRoot, role, loadMore) {
+        const cached = this.inherited.get(role);
+        if (cached && !loadMore) {
+            return cached;
+        }
+
+        const inherited = cached || [];
+        if (inherited.$total == null) {
+            inherited.$total = 0;
+        }
+        this.inherited.set(role, inherited);
+
+        const builder = this.maRole.buildQuery();
         if (isRoot) {
             if (this.filter) {
                 builder.match('name', `*${this.filter}*`);
@@ -77,13 +229,40 @@ class RoleSelectorController {
         }
 
         builder.sort('name')
-            .limit(100, existing.length);
+            .limit(this.limit, inherited.length);
 
         return builder.query().then(roles => {
-            existing.push(...roles);
-            existing.$total = roles.$total;
-            return existing;
+            const mappedFromCache = roles.map(r => this.findRole(r.xid) || r);
+
+            // should not need to sort, roles should be in order from REST API
+            inherited.push(...mappedFromCache);
+            inherited.$total = roles.$total;
+
+            // only set the root here so that all the cache lookups can use the previous root up until this point
+            if (isRoot) {
+                this.root = role;
+            }
+
+            return inherited;
         });
+    }
+
+    reloadChildren(role) {
+        const inherited = this.inherited.get(role);
+        if (inherited) {
+            const builder = this.maRole.buildQuery()
+                .eq('inheritedBy', role.xid || null)
+                .sort('name')
+                .limit(this.limit);
+
+            return builder.query().then(roles => {
+                const mappedFromCache = roles.map(r => this.findRole(r.xid) || r);
+                inherited.length = 0;
+                inherited.push(...mappedFromCache);
+                inherited.$total = roles.$total;
+                return inherited;
+            });
+        }
     }
 
     labelClicked(event, item) {
@@ -113,7 +292,8 @@ export default {
         multiple: '<?ngMultiple',
         disabled: '<?ngDisabled',
         required: '<?ngRequired',
-        disabledOptions: '<?'
+        disabledOptions: '<?',
+        limit: '<?'
     },
     controller: RoleSelectorController,
     template: roleSelectorTemplate
