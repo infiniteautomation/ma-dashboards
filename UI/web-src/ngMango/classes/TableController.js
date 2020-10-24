fe0bae1ebbbe9af1724dc015009489da665e8254
Jared Wiltshire <jazdw@users.noreply.github.com>
2020-10-24 14:11:43 -0600

    Use originalXid property

--- UI/web-src/ngMango/classes/TableController.js
+++ UI/web-src/ngMango/classes/TableController.js
@@ -7,7 +7,7 @@ import BoundedMap from './BoundedMap';
 import moment from 'moment-timezone';
 
 class Column {
-    
+
     constructor(options) {
         Object.assign(this, options);
 
@@ -15,7 +15,7 @@ class Column {
         this.filterable = options.hasOwnProperty('filterable') ? !!options.filterable : true;
         this.sortable = options.hasOwnProperty('sortable') ? !!options.sortable : true;
     }
-    
+
     parseBoolean(value) {
         const lower = value.toLowerCase();
         if (['true', 'y', '1'].includes(lower)) {
@@ -25,22 +25,22 @@ class Column {
         }
         return value;
     }
-    
+
     parseEnum(value) {
         if (/^-?\d+$/.exec(value)) {
             return Number.parseInt(value, 10);
         }
         return value.toUpperCase();
     }
-    
+
     applyFilter(queryBuilder) {
         if (!this.filter) return;
-        
+
         const match = /^(!)?(=|>=|<=|>|<)?(.*)$/.exec(this.filter);
         let invert = !!match[1];
         let op = match[2];
         let value = match[3];
-        
+
         if (value === 'null') {
             value = null;
         } else {
@@ -76,17 +76,17 @@ class Column {
                 op = 'eq';
             }
         }
-        
+
         if (invert && op === 'eq') {
             op = 'ne';
             invert = false;
         }
-        
+
         if (invert) queryBuilder.not();
         queryBuilder[op](this.name, value);
         if (invert) queryBuilder.up();
     }
-    
+
     getValue(item) {
         const property = this.property;
         let result = item;
@@ -110,7 +110,7 @@ class Column {
         }
         return formatted;
     }
-    
+
     getValueAndFormat(item) {
         const value = this.getValue(item);
         if (value === undefined) {
@@ -125,7 +125,7 @@ class TableController {
 
     constructor(options) {
         this.dateFormat = 'dateTime';
-        
+
         Object.assign(this, options);
 
         const $injector = this.$injector;
@@ -154,12 +154,13 @@ class TableController {
         }
 
         this.updateQueue = [];
-        this.deregister = this.resourceService.notificationManager.subscribe((event, item) => {
+        this.deregister = this.resourceService.notificationManager.subscribe((event, item, attributes) => {
             // we queue up the updates and process them in batches to prevent continuous $scope.$apply() when large numbers of
             // items are being edited
             this.updateQueue.push({
                 eventName: event.name,
-                item
+                item,
+                attributes
             });
         });
 
@@ -176,14 +177,14 @@ class TableController {
                 updateQueueLength = this.updateQueue.length;
             }
         }, 500, null, false);
-        
+
         // closes the options menu if this user table is encapsulated in a maDropDown and it closes
         this.$scope.$on('maDropDownClose', event => {
             if (this.mdMenuCtrl) {
                 this.mdMenuCtrl.close(false, {closeAll: true});
             }
         });
-        
+
         // have to load columns first as they may have saved filters which we need to apply before
         // loading the items
         this.loadColumns().then(() => {
@@ -191,15 +192,15 @@ class TableController {
             this.getItems();
         });
     }
-    
+
     $onDestroy() {
         this.deregister();
         this.$interval.cancel(this.intervalPromise);
     }
-    
+
     $onChanges(changes) {
     }
-    
+
     render() {
         let items;
         if (this.selectMultiple) {
@@ -213,7 +214,7 @@ class TableController {
             this.selectedItems.set(item[this.idProperty], item);
         });
     }
-    
+
     setViewValue() {
         if (this.ngModelCtrl) {
             if (this.selectMultiple) {
@@ -229,21 +230,21 @@ class TableController {
             }
         }
     }
-    
+
     loadSettings() {
         this.settings = this.localStorageService.get(this.localStorageKey) || {};
-        
+
         if (this.settings.hasOwnProperty('showFilters')) {
             this.showFilters = !!this.settings.showFilters;
         }
-        
+
         if (!this.settings.filters) {
             this.settings.filters = {};
         }
 
         this.sort = this.settings.sort || this.defaultSort || [];
     }
-    
+
     saveSettings() {
         this.settings.sort = this.sort;
 
@@ -254,10 +255,10 @@ class TableController {
                 this.settings.filters[c.name] = c.filter;
             }
         });
-        
+
         this.localStorageService.set(this.localStorageKey, this.settings);
     }
-    
+
     getFilters() {
         return this.settings.filters;
     }
@@ -276,13 +277,13 @@ class TableController {
             });
         });
     }
-    
+
     selectColumns() {
         const selected = Array.isArray(this.settings.selectedColumns) ? this.settings.selectedColumns : [];
         const deselected = Array.isArray(this.settings.deselectedColumns) ? this.settings.deselectedColumns : [];
         this.selectedColumns = this.columns.filter(c => selected.includes(c.name) || c.selectedByDefault && !deselected.includes(c.name));
     }
-    
+
     markCacheAsStale() {
         for (let page of this.pages.values()) {
             if (page.queryPromise) {
@@ -294,10 +295,10 @@ class TableController {
 
     clearCache(preserveTotal = true) {
         this.markCacheAsStale();
-        
+
         const total = this.pages.$total;
         this.pages = new BoundedMap(this.cacheSize);
-        
+
         // sorting doesn't change the total
         if (preserveTotal) {
             this.pages.$total = total;
@@ -309,7 +310,7 @@ class TableController {
 
         this.customizeQuery(queryBuilder);
         this.selectedColumns.forEach(col => col.applyFilter(queryBuilder));
-        
+
         const sortArray = this.sort.map(item => item.descending ? `-${item.columnName}` : item.columnName);
         if (!this.disableSortById) {
             // ensure the order of the results are deterministic by adding sort on id
@@ -320,7 +321,7 @@ class TableController {
         if (sortArray.length) {
             queryBuilder.sort(...sortArray);
         }
-        
+
         return queryBuilder;
     }
 
@@ -346,9 +347,9 @@ class TableController {
 
         const queryBuilder = this.createQueryBuilder();
         queryBuilder.limit(this.pageSize, startIndex);
-        
+
         page.queryPromise = queryBuilder.query();
-        
+
         page.promise = page.queryPromise.then(result => {
             pages.$total = result.$total;
             delete page.stale;
@@ -356,7 +357,7 @@ class TableController {
             return result;
         }, error => {
             page.error = error;
-            
+
             if (error.status === -1 && error.resource && error.resource.cancelled) {
                 // request cancelled, ignore error
                 pages.delete(startIndex);
@@ -365,24 +366,24 @@ class TableController {
 
             const message = error.mangoStatusText || (error + '');
             this.maDialogHelper.errorToast(['ui.app.errorGettingItems', message]);
-            
+
             // dont remove the page from the cache for 1 minute, stops repetitive requests with errors
             this.$timeout(() => {
                 pages.delete(startIndex);
             }, 60 * 1000);
-            
+
             return this.$q.reject(error);
         }).finally(() => {
             delete page.queryPromise;
             delete page.promise;
         });
-        
+
         return page.promise;
     }
 
     getItems(startIndex = 0) {
         const itemsPromise = this.itemsPromise = this.getPage(startIndex);
-        
+
         this.itemsPromise.finally(() => {
             // check we are deleting our own promise, not one for a new query
             if (this.itemsPromise === itemsPromise) {
@@ -390,10 +391,10 @@ class TableController {
             }
         });
     }
-    
+
     selectAll(startIndex = 0, endIndex = undefined, deselect = false) {
         this.getPage(startIndex, false).then(items => {
-            
+
             items.every((item, i) => {
                 if (endIndex == null || i < endIndex - startIndex) {
                     if (deselect) {
@@ -408,7 +409,7 @@ class TableController {
             const nextPageIndex = startIndex + this.pageSize;
             const hasMore = items.$total > nextPageIndex;
             const wantMore = endIndex == null || endIndex > nextPageIndex;
-            
+
             if (wantMore && hasMore) {
                 this.selectAll(nextPageIndex, endIndex, deselect);
             } else {
@@ -416,7 +417,7 @@ class TableController {
             }
         });
     }
-    
+
     deselectAll(startIndex = 0, endIndex = undefined) {
         return this.selectAll(startIndex, endIndex, true);
     }
@@ -426,7 +427,7 @@ class TableController {
         // a) ascending
         // b) descending
         // c) no sort
-        
+
         const firstSort = this.sort[0];
         if (firstSort && firstSort.columnName === column.name) {
             if (!firstSort.descending) {
@@ -439,7 +440,7 @@ class TableController {
         } else {
             // first click
             this.sort = this.sort.filter(item => item.columnName !== column.name);
-            
+
             this.sort.unshift({columnName: column.name});
             if (this.sort.length > 3) {
                 this.sort.pop();
@@ -455,7 +456,7 @@ class TableController {
         this.settings.deselectedColumns = this.columns
             .filter(c => c.selectedByDefault && !this.selectedColumns.includes(c))
             .map(c => c.name);
-        
+
         this.settings.selectedColumns = this.selectedColumns
             .filter(c => !c.selectedByDefault)
             .map(c => c.name);
@@ -482,7 +483,7 @@ class TableController {
                 queryChanged = true;
             }
         });
-        
+
         if (queryChanged) {
             this.clearCache();
             this.getItems();
@@ -507,7 +508,7 @@ class TableController {
             this.saveSettings();
         }
     }
-    
+
     filterChanged() {
         this.saveSettings();
         this.clearCache();
@@ -517,26 +518,26 @@ class TableController {
     getCellValue(item, column) {
         return column.getValueAndFormat(item);
     }
-    
+
     processUpdateQueue() {
         // TODO we currently have no good way to know if the updated item matches our current query
         // just mark all of our pages as being stale and needing a reload
-        
+
         let setViewValue = false;
-        
+
         while (this.updateQueue.length) {
             const update = this.updateQueue.shift();
-            
+
             if (update.eventName === 'create') {
                 // ignore
             } else if (update.eventName === 'update') {
-                const existing = this.selectedItems.get(update.item[this.idProperty]);
+                const existing = this.selectedItems.get(update.attributes.originalXid);
                 if (existing) {
                     Object.assign(existing, update.item);
                     setViewValue = true;
                 }
             } else if (update.eventName === 'delete') {
-                const deleted = this.selectedItems.delete(update.item[this.idProperty]);
+                const deleted = this.selectedItems.delete(update.attributes.originalXid);
                 if (deleted) {
                     setViewValue = true;
                 }
@@ -559,18 +560,18 @@ class TableController {
         }
         const startIndex = index - index % this.pageSize;
         const page = this.pages.get(startIndex);
-        
+
         if (!page || page.stale) {
             this.getItems(startIndex);
         }
-        
+
         if (page && page.items) {
             return page.items[index - startIndex];
         } else {
             return null;
         }
     }
-    
+
     /**
      * md-virtual-repeat with md-on-demand interface
      */
@@ -582,7 +583,7 @@ class TableController {
         this.selectedItems.clear();
         this.setViewValue();
     }
-    
+
     getSelectedColumnsModel(column) {
         return this.maUtil.createBooleanModel(this.selectedColumns, column, 'name');
     }
@@ -590,7 +591,7 @@ class TableController {
     cancelSelect() {
         delete this.pages.mouseDown;
     }
-    
+
     rowClicked({$event, $item, $index}) {
         this.selectRow($event, $item, $index);
     }
@@ -605,7 +606,7 @@ class TableController {
         if (!this.selectMultiple) {
             this.selectedItems.clear();
         }
-        
+
         if (deselect) {
             this.selectedItems.delete(itemId);
         } else {
@@ -621,15 +622,15 @@ class TableController {
                 return; // dont setViewValue() yet
             }
         }
-        
+
         this.setViewValue();
     }
-    
+
     openMenu(event, mdMenuCtrl) {
         this.mdMenuCtrl = mdMenuCtrl;
         this.mdMenuCtrl.open(event);
     }
-    
+
     createColumn(options) {
         return new Column(options);
     }
