c5b0c7fafcf24496303096e325efa074341101c7
Pier Puccini <pierpuccini@users.noreply.github.com>
2021-01-04 20:27:49 -0500

    Updated default columns

--- UI/web-src/ngMango/components/pointTagCountsTable/pointTagCountsTable.js
+++ UI/web-src/ngMango/components/pointTagCountsTable/pointTagCountsTable.js
@@ -9,13 +9,10 @@ import './pointTagCountsTable.css';
 
 const DEFAULT_COLUMNS = [
     { name: 'xid', label: 'ui.app.xidShort' },
-    // { name: 'dataType', label: 'dsEdit.pointDataType', type: 'enum' },
-    // { name: 'tags.zone', label: 'ui.app.tag', labelArgs: 'zone', selectedByDefault: true },
     { name: 'deviceName', label: 'common.deviceName', selectedByDefault: true },
     { name: 'name', label: 'common.name', selectedByDefault: true },
-    { name: 'message', label: 'ui.app.msg', filterable: false, selectedByDefault: true },
-    // { name: '', filterable: false, sortable: false, selectedByDefault: true },
-    { name: 'level', label: 'ui.app.alarmLvl', filterable: false, selectedByDefault: true },
+    { name: 'message', label: 'ui.app.msg', filterable: false, sortable: false, selectedByDefault: true },
+    { name: 'alarmLevel', label: 'ui.app.alarmLvl', filterable: false, selectedByDefault: true },
     { name: 'value', label: 'ui.app.pointValue', filterable: false, sortable: false, selectedByDefault: true },
     { name: 'count', label: 'ui.app.alarmCounts', type: 'number', filterable: false, selectedByDefault: true }
 ];
